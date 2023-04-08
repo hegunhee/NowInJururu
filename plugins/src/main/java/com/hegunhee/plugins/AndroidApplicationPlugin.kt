26799ca77de2a9c2c0ab34c67139aed5fb7328b7
hegunhee <gunhee0072@gmail.com>
2023-04-08 17:18:10 +0900

    [build-logic] applicationPlugin에 dependencies 설정

--- plugins/src/main/java/com/hegunhee/plugins/AndroidApplicationPlugin.kt
+++ plugins/src/main/java/com/hegunhee/plugins/AndroidApplicationPlugin.kt
@@ -4,6 +4,9 @@ import com.hegunhee.plugins.setup.androidApplication
 import com.hegunhee.plugins.setup.setupAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationPlugin : Plugin<Project> {
 
@@ -22,6 +25,17 @@ class AndroidApplicationPlugin : Plugin<Project> {
                         proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
                     }
                 }
+            }
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+            dependencies {
+                add("implementation",libs.findLibrary("core-ktx").get())
+                add("implementation",libs.findLibrary("appcompat").get())
+                add("implementation",libs.findLibrary("material").get())
+                add("implementation",libs.findLibrary("constraint-layout").get())
+                add("implementation",libs.findLibrary("junit").get())
+                add("androidTestImplementation",libs.findLibrary("ext-junit").get())
+                add("androidTestImplementation",libs.findLibrary("espresso-core").get())
             }
         }
     }
