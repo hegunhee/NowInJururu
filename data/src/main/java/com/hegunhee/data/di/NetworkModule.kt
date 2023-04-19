aaa087b85cd095538c84150266a17c1795888f02
hegunhee <gunhee0072@gmail.com>
2023-04-19 21:49:54 +0900

    [auth-token] Moshi 객체와 Api 인스턴스를 제공하는 Module 생성

---
+++ data/src/main/java/com/hegunhee/data/di/NetworkModule.kt
@@ -0,0 +1,34 @@
+package com.hegunhee.data.di
+
+import com.hegunhee.data.network.TwitchAuthTokenApi
+import com.hegunhee.data.network.TwitchAuthTokenBaseUrl
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi() : Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Singleton
+    @Provides
+    fun provideTwitchAuthTokenApi(
+        moshi : Moshi
+    ) : TwitchAuthTokenApi {
+        return Retrofit.Builder()
+            .baseUrl(TwitchAuthTokenBaseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TwitchAuthTokenApi::class.java)
+    }
+}
