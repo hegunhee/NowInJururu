11acfcbe7f0a7819ac05d3ee6a10a6bcea202b34
hegunhee <gunhee0072@gmail.com>
2023-04-19 21:47:56 +0900

    [auth-token] BuildConfig 변수 테스트

--- data/src/test/java/com/hegunhee/data/ExampleUnitTest.kt
+++ data/src/test/java/com/hegunhee/data/ExampleUnitTest.kt
@@ -14,4 +14,13 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun getClientSecret() {
+        println(BuildConfig.clientId)
+        println(BuildConfig.clientSecret)
+        assert(true)
+    }
+
+
 }
