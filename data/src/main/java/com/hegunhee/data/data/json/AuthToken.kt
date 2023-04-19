bcc283b9c8a0c0ffb729719d1c0b5294313e2ac3
hegunhee <gunhee0072@gmail.com>
2023-04-19 21:48:19 +0900

    [auth-token] authToken 조회 결과로 나오는 data class 정의 및 메서드 정의

---
+++ data/src/main/java/com/hegunhee/data/data/json/AuthToken.kt
@@ -0,0 +1,15 @@
+package com.hegunhee.data.data.json
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class AuthToken(
+    @Json(name = "access_token") val accessToken : String,
+    @Json(name = "expires_in") val expiresIn : Int,
+    @Json(name = "token_type") val tokenType : String
+) {
+    fun getFormattedToken() : String{
+        return "$tokenType $accessToken"
+    }
+}
