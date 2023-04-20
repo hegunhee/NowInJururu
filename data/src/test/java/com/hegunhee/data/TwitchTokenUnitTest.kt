8c2b5d5517693bf0b9312885e302f6d79e69a728
hegunhee <gunhee0072@gmail.com>
2023-04-20 14:00:08 +0900

    [stream-data] Moshi객체나 Retrofit객체의 경우 미리 초기화해 코드 중복을 줄임

--- data/src/test/java/com/hegunhee/data/TwitchTokenUnitTest.kt
+++ data/src/test/java/com/hegunhee/data/TwitchTokenUnitTest.kt
@@ -2,28 +2,34 @@ package com.hegunhee.data
 
 import com.hegunhee.data.network.TwitchAuthTokenApi
 import com.hegunhee.data.network.TwitchAuthTokenBaseUrl
+import com.hegunhee.data.network.TwitchStreamDataApi
+import com.hegunhee.data.network.TwitchStreamDataBaseUrl
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 class TwitchTokenUnitTest {
-
-    @Test
-    fun `get twitch token test`() {
-
-        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-        val retrofit = Retrofit.Builder().baseUrl(TwitchAuthTokenBaseUrl)
+    private lateinit var moshi : Moshi
+    private lateinit var tokenApi : TwitchAuthTokenApi
+    @Before
+    fun initMoshiAndRetrofit()  {
+        moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+        tokenApi = Retrofit.Builder().baseUrl(TwitchAuthTokenBaseUrl)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(TwitchAuthTokenApi::class.java)
+    }
+    @Test
+    fun `get twitch token test`() {
 
         runBlocking {
             runCatching {
-                retrofit.getAuthToken()
+                tokenApi.getAuthToken()
             }.onSuccess {
                 println("authToken = $it")
                 println("tokenFormat = ${it.getFormattedToken()}")
