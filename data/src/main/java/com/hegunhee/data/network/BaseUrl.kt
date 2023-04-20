1ab0282a6ac846bd683cbbd19134d8c875b1cd24
hegunhee <gunhee0072@gmail.com>
2023-04-20 09:54:01 +0900

    [stream-data] BaseUrl과 Api interface 정의

--- data/src/main/java/com/hegunhee/data/network/BaseUrl.kt
+++ data/src/main/java/com/hegunhee/data/network/BaseUrl.kt
@@ -1,3 +1,5 @@
 package com.hegunhee.data.network
 
 const val TwitchAuthTokenBaseUrl = "https://id.twitch.tv/"
+
+const val TwitchStreamDataBaseUrl = "https://api.twitch.tv/helix/"
---
+++ data/src/main/java/com/hegunhee/data/network/TwitchStreamDataApi.kt
@@ -0,0 +1,17 @@
+package com.hegunhee.data.network
+
+import com.hegunhee.data.BuildConfig
+import com.hegunhee.data.data.json.StreamDataResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface TwitchStreamDataApi {
+
+    @GET("streams")
+    suspend fun getStreamData(
+        @Header("client-id") clientId : String = BuildConfig.clientId,
+        @Header("Authorization") Authorization : String,
+        @Query("user_login") userLogin : String = "viichan6"
+    ) : StreamDataResponse
+}
