5a27b494b9a8ff94209b485035913b5e07540e72
hegunhee <gunhee0072@gmail.com>
2023-04-20 14:00:08 +0900

    [stream-data] Moshi객체나 Retrofit객체의 경우 미리 초기화해 코드 중복을 줄임

--- data/src/test/java/com/hegunhee/data/TwitchStreamDataResponseUnitTest.kt
+++ data/src/test/java/com/hegunhee/data/TwitchStreamDataResponseUnitTest.kt
@@ -7,31 +7,37 @@ import com.hegunhee.data.network.TwitchStreamDataBaseUrl
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class TwitchStreamDataResponseUnitTest {
 
-    @Test
-    fun `get Stream Data`() {
-        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-        val tokenRetrofit = Retrofit.Builder().baseUrl(TwitchAuthTokenBaseUrl)
+    private lateinit var moshi : Moshi
+    private lateinit var tokenApi : TwitchAuthTokenApi
+    private lateinit var streamDataApi : TwitchStreamDataApi
+
+    @Before
+    fun initMoshiAndRetrofit()  {
+        moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+        tokenApi = Retrofit.Builder().baseUrl(TwitchAuthTokenBaseUrl)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(TwitchAuthTokenApi::class.java)
-
-
-        val StreamDataRetrofit = Retrofit.Builder().baseUrl(TwitchStreamDataBaseUrl)
+        streamDataApi = Retrofit.Builder().baseUrl(TwitchStreamDataBaseUrl)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(TwitchStreamDataApi::class.java)
+    }
 
+    @Test
+    fun `get Stream Data`() {
         runBlocking {
             runCatching {
-                val token = tokenRetrofit.getAuthToken().accessToken
+                val token = tokenApi.getAuthToken()
                 println("Bearer $token")
-                StreamDataRetrofit.getStreamData(Authorization = "Bearer $token")
+                streamDataApi.getStreamData(Authorization = token.getFormattedToken())
             }.onSuccess {
                 println("authToken = $it")
                 assert(true)
