fcbc22966c5b8e1e173e2f13139da8532a1a9b72
hegunhee <gunhee0072@gmail.com>
2023-04-21 21:29:44 +0900

    [stream-data] domain layer와 ui layer에서 사용할 스트림데이터 데이터클래스와 매퍼

---
+++ domain/src/main/java/com/hegunhee/domain/model/StreamData.kt
@@ -0,0 +1,30 @@
+package com.hegunhee.domain.model
+
+/**
+ * EmptyData에는 Streamer Info 정보들이 담길예정
+ */
+
+sealed class StreamDataType() {
+    data class StreamData(
+        val gameId: String,
+        val gameName: String,
+        val id: String,
+        val isMature: Boolean,
+        val language: String,
+        val startedAt: String,
+        val tags: List<String> = emptyList(),
+        val thumbnailUrl: String,
+        val title: String,
+        val type: String,
+        val userId: String,
+        val userLogin: String,
+        val userName: String,
+        val viewerCount: Int
+    ) : StreamDataType()
+
+    object EmptyData : StreamDataType() {
+        override fun toString(): String {
+            return "EmptyData"
+        }
+    }
+}
