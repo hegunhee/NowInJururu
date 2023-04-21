1a32bdaee7af7c3a21e44bc0b39666cae9f9870c
hegunhee <gunhee0072@gmail.com>
2023-04-21 21:29:44 +0900

    [stream-data] domain layer와 ui layer에서 사용할 스트림데이터 데이터클래스와 매퍼

---
+++ data/src/main/java/com/hegunhee/data/mapper/ModelMapper.kt
@@ -0,0 +1,23 @@
+package com.hegunhee.data.mapper
+
+import com.hegunhee.data.data.json.StreamApiData
+import com.hegunhee.domain.model.StreamDataType.StreamData
+
+fun StreamApiData.toStreamData(): StreamData {
+    return StreamData(
+        gameId = gameId,
+        gameName = gameName,
+        id = id,
+        isMature = isMature,
+        language = language,
+        startedAt = startedAt,
+        tags = tags,
+        thumbnailUrl = thumbnailUrl,
+        title = title,
+        type = type,
+        userId = userId,
+        userLogin = userLogin,
+        userName = userName,
+        viewerCount = viewerCount
+    )
+}
