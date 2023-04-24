a40e7ab3e6aa8cc024a3097df228e15ffcd9d212
hegunhee <gunhee0072@gmail.com>
2023-04-24 15:04:03 +0900

    [NavigationUI] JururuFragment와 xml 작성

---
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/jururu/JururuFragment.kt
@@ -0,0 +1,32 @@
+package com.hegunhee.feature.streamer.jururu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hegunhee.feature.streamer.R
+import com.hegunhee.feature.streamer.databinding.FragmentJururuBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class JururuFragment : Fragment() {
+
+    private lateinit var viewDataBinding : FragmentJururuBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_jururu,container,false)
+        viewDataBinding = FragmentJururuBinding.bind(root).apply {
+
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
