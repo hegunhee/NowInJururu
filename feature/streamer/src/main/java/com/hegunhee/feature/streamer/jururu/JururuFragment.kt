4939270e033040d615551c91aca0e1c6e67eefc8
hegunhee <gunhee0072@gmail.com>
2023-04-25 14:55:25 +0900

    [JururuFragment] JururuFragment와 xml파일 작성

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/jururu/JururuFragment.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/jururu/JururuFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hegunhee.domain.model.StreamDataType
 import com.hegunhee.feature.streamer.R
+import com.hegunhee.feature.streamer.StreamerAdapter
 import com.hegunhee.feature.streamer.databinding.FragmentJururuBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class JururuFragment : Fragment() {
 
     private lateinit var viewDataBinding : FragmentJururuBinding
+    private lateinit var streamerAdapter : StreamerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,13 +23,16 @@ class JururuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_jururu,container,false)
+        streamerAdapter = StreamerAdapter()
         viewDataBinding = FragmentJururuBinding.bind(root).apply {
-
+            JururuRecyclerView.adapter = streamerAdapter
+            lifecycleOwner = viewLifecycleOwner
         }
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        streamerAdapter.submitList(listOf<StreamDataType>(StreamDataType.EmptyData,StreamDataType.EmptyData,StreamDataType.EmptyData,StreamDataType.EmptyData,StreamDataType.EmptyData))
     }
 }
