6a2d98f159bf216c53d2e4db8290f379357011a9
hegunhee <gunhee0072@gmail.com>
2023-04-25 14:57:16 +0900

    [JururuFragment] StreamerAdapter 작성

---
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerAdapter.kt
@@ -0,0 +1,67 @@
+package com.hegunhee.feature.streamer
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import com.hegunhee.domain.model.StreamDataType
+import com.hegunhee.feature.streamer.databinding.ItemLiveStreamerBinding
+import com.hegunhee.feature.streamer.databinding.ItemUnLiveStreamerBinding
+
+class StreamerAdapter() : ListAdapter<StreamDataType,StreamerAdapter.StreamerAdapterViewHolder>(DiffUtil) {
+
+    sealed class StreamerAdapterViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        abstract fun bindView(memo: StreamDataType)
+
+    }
+
+    inner class LiveStreamerViewHolder(private val binding : ItemLiveStreamerBinding) : StreamerAdapterViewHolder(binding.root){
+        override fun bindView(memo: StreamDataType) {
+
+        }
+    }
+
+    inner class UnLiveStreamerViewHolder(private val binding : ItemUnLiveStreamerBinding) : StreamerAdapterViewHolder(binding.root){
+        override fun bindView(memo: StreamDataType) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StreamerAdapterViewHolder {
+        return when(viewType){
+            R.layout.item_live_streamer ->{
+                LiveStreamerViewHolder(ItemLiveStreamerBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+            }
+            R.layout.item_un_live_streamer -> {
+                UnLiveStreamerViewHolder(ItemUnLiveStreamerBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+            }
+            else -> { throw IllegalArgumentException()}
+        }
+    }
+
+    override fun onBindViewHolder(holder: StreamerAdapterViewHolder, position: Int) {
+        holder.bindView(getItem(position))
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)){
+            is StreamDataType.StreamData -> R.layout.item_live_streamer
+            is StreamDataType.EmptyData -> R.layout.item_un_live_streamer
+        }
+    }
+
+}
+
+internal object DiffUtil : DiffUtil.ItemCallback<StreamDataType>(){
+
+    override fun areItemsTheSame(oldItem: StreamDataType, newItem: StreamDataType): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: StreamDataType, newItem: StreamDataType): Boolean {
+        return oldItem == newItem
+    }
+
+}
