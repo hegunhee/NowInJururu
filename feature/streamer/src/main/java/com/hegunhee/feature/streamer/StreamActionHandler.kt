90b426252a861799bb177e1d2c02482a97cab0d5
hegunhee <gunhee0072@gmail.com>
2023-04-26 13:21:53 +0900

    [JururuFragment] JururuViewModel과 ActionHandler 작성

---
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamActionHandler.kt
@@ -0,0 +1,4 @@
+package com.hegunhee.feature.streamer
+
+interface StreamActionHandler {
+}
--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/jururu/JururuViewModel.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/jururu/JururuViewModel.kt
@@ -1,8 +1,29 @@
 package com.hegunhee.feature.streamer.jururu
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hegunhee.domain.model.StreamDataType
+import com.hegunhee.domain.usecase.GetJururuStreamDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class JururuViewModel : ViewModel() {
+class JururuViewModel @Inject constructor(private val getJururuStreamDataUseCase: GetJururuStreamDataUseCase) : ViewModel() {
+
+    private val _jururuStreamData : MutableStateFlow<StreamDataType> = MutableStateFlow(StreamDataType.EmptyData("","",""))
+    val jururuStreamData : StateFlow<StreamDataType> = _jururuStreamData.asStateFlow()
+
+    fun getJururuStreamData() {
+        viewModelScope.launch {
+            getJururuStreamDataUseCase()
+                .onSuccess {
+                    _jururuStreamData.emit(it)
+                }
+                .onFailure {  }
+        }
+    }
 }
