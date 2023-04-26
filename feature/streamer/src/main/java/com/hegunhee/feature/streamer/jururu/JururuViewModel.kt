78712f0b2d8e3b7568cf2d0c171c51b2c561ae33
hegunhee <gunhee0072@gmail.com>
2023-04-26 10:15:35 +0900

    [JururuFragment] JururuViewmodel 생성

---
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/jururu/JururuViewModel.kt
@@ -0,0 +1,8 @@
+package com.hegunhee.feature.streamer.jururu
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class JururuViewModel : ViewModel() {
+}
