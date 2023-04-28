ee6f2ec9ff2e746f14dbc0b86f5a8753bfc87cd9
hegunhee <gunhee0072@gmail.com>
2023-04-28 15:29:37 +0900

    [TwitchDeepLink] MainActivity 사용안하는 코드 정리

--- feature/main/src/main/java/com/hegunhee/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/hegunhee/feature/main/MainActivity.kt
@@ -31,48 +31,6 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewDataBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
-        Log.d("TEST!!!",testUseCase().toString())
-        lifecycleScope.launchWhenStarted {
-            Log.d("TEST!!!",getStreamDataUseCase().getOrNull()!!.toString())
-        }
-
-
-
-//        button.setOnClickListener {
-//            val intent = packageManager.getLaunchIntentForPackage("tv.twitch.android.app")
-//            if(intent != null){
-//                it
-//            }else{
-//                it
-//            }
-//            Intent(Intent.ACTION_VIEW).apply {
-//                data = Uri.parse("twitch://stream/cotton__123")
-//                startActivity(this)
-//            }
-
-//            val intent = packageManager.getLaunchIntentForPackage("tv.twitch.android.app")
-//            intent?.data = Uri.parse("twitch://stream/cotton__123")
-//            startActivity(intent)
-//            runCatching {
-//                packageManager.getPackageInfo("tv.twitch.android.app", PackageManager.PackageInfoFlags.of(0L))
-//            }.onSuccess {
-//                it
-//                Intent().apply {
-//                    data = Uri.parse("twitch://stream/cotton__123")
-//                    startActivity(this)
-//                }
-//            }.onFailure {
-//                it
-//            }
-//            if (intent != null) {
-//                intent.data = Uri.parse("twitch://stream/cotton__123")
-//                startActivity(intent)
-//            } else {
-//                // 트위치 앱이 설치되어 있지 않은 경우 구글 플레이 스토어 페이지로 이동합니다.
-////                val elseIntent = Intent(Intent.ACTION_VIEW)
-////                elseIntent.data = Uri.parse("market://details?id=tv.twitch.android.app")
-////                startActivity(elseIntent)
-//            }
+        viewDataBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
