87d25f87b6105217dae531b195613fd4fd278398
hegunhee <gunhee0072@gmail.com>
2023-04-28 15:25:52 +0900

    [TwitchDeepLink] 트위치 스트리머 클릭 함수 viewModel에서 구현

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/jururu/JururuViewModel.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/jururu/JururuViewModel.kt
@@ -4,18 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hegunhee.domain.model.StreamDataType
 import com.hegunhee.domain.usecase.GetJururuStreamDataUseCase
+import com.hegunhee.feature.streamer.StreamActionHandler
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class JururuViewModel @Inject constructor(private val getJururuStreamDataUseCase: GetJururuStreamDataUseCase) : ViewModel() {
+class JururuViewModel @Inject constructor(private val getJururuStreamDataUseCase: GetJururuStreamDataUseCase) : ViewModel(), StreamActionHandler {
 
     private val _jururuStreamData : MutableStateFlow<StreamDataType> = MutableStateFlow(StreamDataType.EmptyData("","",""))
     val jururuStreamData : StateFlow<StreamDataType> = _jururuStreamData.asStateFlow()
+
+    private val _navigateStreamerTwitch : MutableSharedFlow<String> = MutableSharedFlow()
+    val navigateStreamerTwitch : SharedFlow<String> = _navigateStreamerTwitch.asSharedFlow()
 
     fun getJururuStreamData() {
         viewModelScope.launch {
@@ -24,6 +26,12 @@ class JururuViewModel @Inject constructor(private val getJururuStreamDataUseCase
                     _jururuStreamData.emit(it)
                 }
                 .onFailure {  }
+        }
+    }
+
+    override fun onClickTwitchStreamerItem(streamerLogin: String) {
+        viewModelScope.launch {
+            _navigateStreamerTwitch.emit(streamerLogin)
         }
     }
 }
