0bea62e6b1cb1e66f09a69738fb2091ed22d66df
hegunhee <gunhee0072@gmail.com>
2023-04-28 15:24:00 +0900

    [TwitchDeepLink] 딥링크를 탐색하거나 앱 패키지 검색할때 사용하는 확장함수 정의

---
+++ feature/common/src/main/java/com/hegunhee/feature/common/twitch/TwitchUtil.kt
@@ -0,0 +1,30 @@
+package com.hegunhee.feature.common.twitch
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.net.Uri
+import com.hegunhee.feature.common.R
+
+fun Context.isInstalledTwitchAppOrException() : PackageInfo {
+    return packageManager.getPackageInfo(getString(R.string.twitchPackageName), PackageManager.PackageInfoFlags.of(0L))
+}
+
+fun Context.openStreamerStream(streamerLogin : String) {
+    Intent(Intent.ACTION_VIEW).apply {
+        data = streamerLogin.toStreamerUri(this@openStreamerStream)
+        startActivity(this)
+    }
+}
+
+fun Context.openPlayStore() {
+    Intent(Intent.ACTION_VIEW).apply {
+        data = Uri.parse(getString(R.string.playStoreTwitchDeepLink))
+        startActivity(this)
+    }
+}
+
+fun String.toStreamerUri(context : Context) : Uri {
+    return Uri.parse("${context.getString(R.string.twitchStreamBaseUrl)}$this")
+}
