288e5748a4d5da84eec590c4291a95fb1e73e232
hegunhee <gunhee0072@gmail.com>
2023-05-04 14:58:34 +0900

    [StreamerSearchApi] adapter와 layoutManager설정

--- feature/search/src/main/java/com/hegunhee/feature/search/SearchFragment.kt
+++ feature/search/src/main/java/com/hegunhee/feature/search/SearchFragment.kt
@@ -6,14 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.hegunhee.feature.search.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchFragment : Fragment(){
 
     private lateinit var viewDataBinding : FragmentSearchBinding
     private val viewModel : SearchViewModel by viewModels()
+    private lateinit var searchAdapter : SearchAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,8 +25,10 @@ class SearchFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_search,container,false)
+        searchAdapter = SearchAdapter(viewModel)
         viewDataBinding = FragmentSearchBinding.bind(root).apply {
             viewModel = this@SearchFragment.viewModel
+            this.SearchResultRecyclerView.adapter = searchAdapter
             lifecycleOwner = viewLifecycleOwner
         }
         return root
@@ -30,6 +36,16 @@ class SearchFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeData()
+    }
 
+    private fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            launch {
+                viewModel.searchResult.collect{
+                    searchAdapter.submitList(it)
+                }
+            }
+        }
     }
 }
