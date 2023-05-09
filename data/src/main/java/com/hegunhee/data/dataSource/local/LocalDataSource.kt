b34c73faf35df8c3dcfe30727d642330ead45f61
hegunhee <gunhee0072@gmail.com>
2023-05-09 20:38:14 +0900

    [RoomDatabase] LocalDataSource 메서드 정의

--- data/src/main/java/com/hegunhee/data/dataSource/local/LocalDataSource.kt
+++ data/src/main/java/com/hegunhee/data/dataSource/local/LocalDataSource.kt
@@ -1,4 +1,12 @@
 package com.hegunhee.data.dataSource.local
 
+import com.hegunhee.data.database.entity.StreamerEntity
+
 interface LocalDataSource {
+
+    suspend fun insertStreamer(streamerEntity: StreamerEntity)
+
+    suspend fun updateStreamer(streamerEntity: StreamerEntity)
+
+    suspend fun getAllStreamerList(): List<StreamerEntity>
 }
