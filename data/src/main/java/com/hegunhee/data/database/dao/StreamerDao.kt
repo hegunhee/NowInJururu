9b74fc072998fdafec8e80039cbf3c44effb6135
hegunhee <gunhee0072@gmail.com>
2023-05-09 15:11:07 +0900

    [RoomDatabase] entity 추가, 수정, 전체 조회 Dao 메서드 작성

--- data/src/main/java/com/hegunhee/data/database/dao/StreamerDao.kt
+++ data/src/main/java/com/hegunhee/data/database/dao/StreamerDao.kt
@@ -1,4 +1,20 @@
 package com.hegunhee.data.database.dao
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.hegunhee.data.database.entity.StreamerEntity
+
 interface StreamerDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertStreamer(streamerEntity: StreamerEntity)
+
+    @Update
+    suspend fun updateStreamer(streamerEntity: StreamerEntity)
+
+    @Query("SELECT * FROM StreamerEntity")
+    suspend fun getAllStreamerEntityList() : List<StreamerEntity>
+    
 }
