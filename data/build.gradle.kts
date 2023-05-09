d3346005e910b8f56eab32cf8e97d74f43790106
hegunhee <gunhee0072@gmail.com>
2023-05-09 14:36:19 +0900

    [RoomDatabase] 디펜던시 코드 toml파일에서 정의한 디펜던시를 가져오는것으로 수정

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -21,9 +21,9 @@ dependencies {
 
     implementation(project(":domain"))
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 
     implementation(libs.bundles.retrofit2)
     implementation(libs.bundles.moshi)
