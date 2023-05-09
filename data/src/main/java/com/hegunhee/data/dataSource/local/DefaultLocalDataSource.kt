421a8967be40def8c6fc8f8f7c69714b4460fb4f
hegunhee <gunhee0072@gmail.com>
2023-05-09 20:39:09 +0900

    [RoomDatabase] LocalDataSource 메서드 구현

--- data/src/main/java/com/hegunhee/data/dataSource/local/DefaultLocalDataSource.kt
+++ data/src/main/java/com/hegunhee/data/dataSource/local/DefaultLocalDataSource.kt
@@ -1,4 +1,19 @@
 package com.hegunhee.data.dataSource.local
 
-class DefaultLocalDataSource : LocalDataSource {
+import com.hegunhee.data.database.dao.StreamerDao
+import com.hegunhee.data.database.entity.StreamerEntity
+import javax.inject.Inject
+
+class DefaultLocalDataSource @Inject constructor(private val streamerDao : StreamerDao) : LocalDataSource {
+    override suspend fun insertStreamer(streamerEntity: StreamerEntity) {
+        streamerDao.insertStreamer(streamerEntity)
+    }
+
+    override suspend fun updateStreamer(streamerEntity: StreamerEntity) {
+        streamerDao.updateStreamer(streamerEntity)
+    }
+
+    override suspend fun getAllStreamerList(): List<StreamerEntity> {
+        return streamerDao.getAllStreamerEntityList()
+    }
 }
