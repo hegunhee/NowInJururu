f8d625c51718473c714fecde22022e283dd6220c
hegunhee <gunhee0072@gmail.com>
2023-05-09 20:29:38 +0900

    [RoomDatabase] LocalDataSource interface와 구현체 생성

---
+++ data/src/main/java/com/hegunhee/data/dataSource/local/DefaultLocalDataSource.kt
@@ -0,0 +1,4 @@
+package com.hegunhee.data.dataSource.local
+
+class DefaultLocalDataSource : LocalDataSource {
+}
---
+++ data/src/main/java/com/hegunhee/data/dataSource/local/LocalDataSource.kt
@@ -0,0 +1,4 @@
+package com.hegunhee.data.dataSource.local
+
+interface LocalDataSource {
+}
