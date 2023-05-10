e43f5f918515d9daa976f12243b90eb60e76c72a
hegunhee <gunhee0072@gmail.com>
2023-05-10 13:02:48 +0900

    [SaveStreamer] Dao 인터페이스에 Dao 어노테이션 추가

--- data/src/main/java/com/hegunhee/data/database/dao/StreamerDao.kt
+++ data/src/main/java/com/hegunhee/data/database/dao/StreamerDao.kt
@@ -1,11 +1,13 @@
 package com.hegunhee.data.database.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.hegunhee.data.database.entity.StreamerEntity
 
+@Dao
 interface StreamerDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
