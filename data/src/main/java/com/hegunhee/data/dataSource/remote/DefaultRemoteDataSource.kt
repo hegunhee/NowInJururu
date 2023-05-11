7c52d8d12742e03868230dab3a5770c4377f2c5d
hegunhee <gunhee0072@gmail.com>
2023-05-11 15:28:35 +0900

    [StreamerUI] RemoteDataSource에서 스트리머 api 데이터를 가져오는 함수 구현

--- data/src/main/java/com/hegunhee/data/dataSource/remote/DefaultRemoteDataSource.kt
+++ data/src/main/java/com/hegunhee/data/dataSource/remote/DefaultRemoteDataSource.kt
@@ -3,15 +3,18 @@ package com.hegunhee.data.dataSource.remote
 import com.hegunhee.data.data.json.AuthToken
 import com.hegunhee.data.data.json.SearchApiDataResponse
 import com.hegunhee.data.data.json.StreamApiDataResponse
+import com.hegunhee.data.data.json.StreamerApiDataResponse
 import com.hegunhee.data.network.TwitchAuthTokenApi
 import com.hegunhee.data.network.TwitchSearchDataApi
 import com.hegunhee.data.network.TwitchStreamDataApi
+import com.hegunhee.data.network.TwitchStreamerDataApi
 import javax.inject.Inject
 
 class DefaultRemoteDataSource @Inject constructor(
     private val twitchAuthTokenApi : TwitchAuthTokenApi,
     private val twitchStreamDataApi: TwitchStreamDataApi,
-    private val twitchSearchDataApi : TwitchSearchDataApi
+    private val twitchSearchDataApi : TwitchSearchDataApi,
+    private val twitchStreamerDataApi: TwitchStreamerDataApi
     )
     : RemoteDataSource {
     override suspend fun getAuthToken(): AuthToken {
@@ -27,6 +30,13 @@ class DefaultRemoteDataSource @Inject constructor(
         token: String
     ): SearchApiDataResponse {
         return twitchSearchDataApi.getSearchData(streamerName = streamerName, authorization = token)
+    }
+
+    override suspend fun getStreamerDataResponse(
+        vararg streamerLogin: String,
+        token: String
+    ): StreamerApiDataResponse {
+        return twitchStreamerDataApi.getStreamerData(userLogin = streamerLogin, authorization = token)
     }
 
 
