41b357f38967a34e88fde26a1a217506c13d9d65
hegunhee <gunhee0072@gmail.com>
2023-05-11 14:33:58 +0900

    [StreamerUI] 한명의 스트리머 정보 탐색 Unit Test 작성

--- data/src/test/java/com/hegunhee/data/TwitchStreamerDataResponseUnitTest.kt
+++ data/src/test/java/com/hegunhee/data/TwitchStreamerDataResponseUnitTest.kt
@@ -1,4 +1,55 @@
 package com.hegunhee.data
 
+import com.hegunhee.data.network.TwitchAuthTokenApi
+import com.hegunhee.data.network.TwitchAuthTokenBaseUrl
+import com.hegunhee.data.network.TwitchGetBaseUrl
+import com.hegunhee.data.network.TwitchStreamerDataApi
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 class TwitchStreamerDataResponseUnitTest {
+
+    private lateinit var moshi : Moshi
+    private lateinit var tokenApi : TwitchAuthTokenApi
+    private lateinit var streamerApi : TwitchStreamerDataApi
+
+    @Before
+    fun initMoshiAndApi() {
+        moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+        tokenApi = Retrofit.Builder().baseUrl(TwitchAuthTokenBaseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TwitchAuthTokenApi::class.java)
+        streamerApi = Retrofit.Builder().baseUrl(TwitchGetBaseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TwitchStreamerDataApi::class.java)
+    }
+
+    @Test
+    fun `test one streamer data (Jururu)`() {
+        runBlocking {
+            runCatching {
+                val streamer = listOf<String>("cotton__123")
+                val token = tokenApi.getAuthToken().getFormattedToken()
+                println(streamer)
+                streamerApi.getStreamerData(authorization = token,userLogin =streamer.toTypedArray())
+            }.onSuccess {response ->
+                println(response.toString())
+                if(response.streamerApiDataList[0].display_name == "주르르"){
+                    assert(true)
+                }else{
+                    assert(false)
+                }
+            }.onFailure {
+                println(it.message)
+                assert(false)
+            }
+        }
+    }
 }
