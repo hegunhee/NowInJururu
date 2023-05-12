5b095ac57b5064ab5cb9b23224acbed5cc371254
hegunhee <gunhee0072@gmail.com>
2023-05-12 10:13:57 +0900

    [StreamUI] 현재 저장중인 스트리머의 방송 정보를 불러오는 UseCase 생성

---
+++ domain/src/main/java/com/hegunhee/domain/usecase/GetBookmarkedStreamDataListUseCase.kt
@@ -0,0 +1,12 @@
+package com.hegunhee.domain.usecase
+
+import com.hegunhee.domain.model.StreamDataType
+import com.hegunhee.domain.repository.Repository
+import javax.inject.Inject
+
+class GetBookmarkedStreamDataListUseCase @Inject constructor(private val repository: Repository) {
+
+    suspend operator fun invoke() : Result<List<StreamDataType>>{
+        return repository.getStreamDataList()
+    }
+}
