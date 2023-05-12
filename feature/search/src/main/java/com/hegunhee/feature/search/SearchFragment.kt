685bee3e0d62e8be0be73973bbae6629d4288a57
hegunhee <gunhee0072@gmail.com>
2023-05-12 09:54:05 +0900

    [StreamerUI] 스트리머 저장에 실패했다면 스트리머 재검색이 아닌 toast메시지 전달하도록 변경

--- feature/search/src/main/java/com/hegunhee/feature/search/SearchFragment.kt
+++ feature/search/src/main/java/com/hegunhee/feature/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -58,6 +59,11 @@ class SearchFragment : Fragment(){
                     }.onFailure {
                         requireContext().openPlayStore()
                     }
+                }
+            }
+            launch{
+                viewModel.toastMessage.collect{ message ->
+                    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
                 }
             }
         }
