61b1016d68292eb2c9382e004597c0fb14d53582
hegunhee <gunhee0072@gmail.com>
2023-05-13 21:17:31 +0900

    [StreamerUI] StreamerViewModel 클래스 생성

---
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerViewModel.kt
@@ -0,0 +1,9 @@
+package com.hegunhee.feature.streamer
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StreamerViewModel @Inject constructor() : ViewModel() {
+}
