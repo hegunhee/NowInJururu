4b91344ed61c789229988b2a951c5562811a2fa1
hegunhee <gunhee0072@gmail.com>
2023-05-13 20:51:55 +0900

    [StreamerUI] MainActivity의 사용하지않는 코드 정리

--- feature/main/src/main/java/com/hegunhee/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/hegunhee/feature/main/MainActivity.kt
@@ -1,32 +1,17 @@
 package com.hegunhee.feature.main
 
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.lifecycleScope
-import com.hegunhee.domain.Test.TestUseCase
-import com.hegunhee.domain.usecase.GetStreamDataUseCase
 import com.hegunhee.feature.main.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewDataBinding : ActivityMainBinding
-
-    @Inject lateinit var testUseCase : TestUseCase
-
-    @Inject lateinit var getStreamDataUseCase : GetStreamDataUseCase
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
