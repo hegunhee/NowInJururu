d514ffc8c27f494a6ac7e401703687de83f121c9
hegunhee <gunhee0072@gmail.com>
2023-05-15 15:23:31 +0900

    [StreamerUI] viewModel에서 현재 저장된 스트리머 정보들 받아오는 코드 작성

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerViewModel.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerViewModel.kt
@@ -1,9 +1,30 @@
 package com.hegunhee.feature.streamer
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hegunhee.domain.model.StreamDataType
+import com.hegunhee.domain.usecase.GetBookmarkedStreamDataListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class StreamerViewModel @Inject constructor() : ViewModel() {
+class StreamerViewModel @Inject constructor(
+    private val getBookmarkedStreamDataListUseCase: GetBookmarkedStreamDataListUseCase
+) : ViewModel() {
+
+    private val _streamDataList : MutableStateFlow<List<StreamDataType>> = MutableStateFlow(emptyList())
+    val streamDataList : StateFlow<List<StreamDataType>> = _streamDataList.asStateFlow()
+
+    fun fetchBookmarkedStreamData() = viewModelScope.launch{
+        getBookmarkedStreamDataListUseCase()
+            .onSuccess {
+                _streamDataList.emit(it)
+            }.onFailure {
+                
+            }
+    }
 }
