c4263b69f6686cdc861997471a02e15854dc80c2
hegunhee <gunhee0072@gmail.com>
2023-05-15 15:50:12 +0900

    [StreamerUI] streamerFragment에서 viewModel에서 데이터 옵저빙후 처리 로직 구현

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerFragment.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerFragment.kt
@@ -6,14 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.hegunhee.feature.common.twitch.isInstalledTwitchAppOrException
+import com.hegunhee.feature.common.twitch.openPlayStore
+import com.hegunhee.feature.common.twitch.openStreamerStream
 import com.hegunhee.feature.streamer.databinding.FragmentStreamerBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class StreamerFragment : Fragment() {
 
     private lateinit var viewDataBinding : FragmentStreamerBinding
     private val viewModel : StreamerViewModel by viewModels()
+    private lateinit var streamerAdapter: StreamerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,9 +28,39 @@ class StreamerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_streamer,container,false)
+        streamerAdapter = StreamerAdapter(viewModel)
         viewDataBinding = FragmentStreamerBinding.bind(root).apply {
-
+            streamerRecyclerView.adapter = streamerAdapter
+            lifecycleOwner = viewLifecycleOwner
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.fetchBookmarkedStreamData()
+        observeData()
+    }
+
+    private fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            launch {
+                viewModel.streamDataList.collect{
+                    streamerAdapter.submitList(it)
+                }
+            }
+            launch {
+                viewModel.navigateStreamerTwitch.collect{ streamerLogin ->
+                    runCatching {
+                        requireContext().isInstalledTwitchAppOrException()
+                    }.onSuccess {
+                        requireContext().openStreamerStream(streamerLogin)
+                    }.onFailure {
+                        requireContext().openPlayStore()
+                    }
+                }
+
+            }
+        }
     }
 }
