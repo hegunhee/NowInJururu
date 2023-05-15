682f1174b181de28413b68007ff0290d89975602
hegunhee <gunhee0072@gmail.com>
2023-05-15 15:46:51 +0900

    [StreamerUI] StreamerViewModel에서 streamActionHandler 구현 및 메서드 명세 작성

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerViewModel.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerViewModel.kt
@@ -5,26 +5,33 @@ import androidx.lifecycle.viewModelScope
 import com.hegunhee.domain.model.StreamDataType
 import com.hegunhee.domain.usecase.GetBookmarkedStreamDataListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class StreamerViewModel @Inject constructor(
     private val getBookmarkedStreamDataListUseCase: GetBookmarkedStreamDataListUseCase
-) : ViewModel() {
+) : ViewModel(), StreamActionHandler {
 
     private val _streamDataList : MutableStateFlow<List<StreamDataType>> = MutableStateFlow(emptyList())
     val streamDataList : StateFlow<List<StreamDataType>> = _streamDataList.asStateFlow()
+
+    private val _navigateStreamerTwitch : MutableSharedFlow<String> = MutableSharedFlow()
+    val navigateStreamerTwitch : SharedFlow<String> = _navigateStreamerTwitch.asSharedFlow()
 
     fun fetchBookmarkedStreamData() = viewModelScope.launch{
         getBookmarkedStreamDataListUseCase()
             .onSuccess {
                 _streamDataList.emit(it)
             }.onFailure {
-                
+
             }
+    }
+
+    override fun onClickTwitchStreamerItem(streamerLogin: String) {
+        viewModelScope.launch {
+            _navigateStreamerTwitch.emit(streamerLogin)
+        }
     }
 }
