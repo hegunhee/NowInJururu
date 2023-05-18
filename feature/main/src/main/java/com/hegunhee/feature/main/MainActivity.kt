419ff06f7e063c8c6cc75ee93c0b9bdbd069e906
hegunhee <gunhee0072@gmail.com>
2023-05-18 13:11:09 +0900

    [StreamerUI] add. 다크모드 비활성화

--- feature/main/src/main/java/com/hegunhee/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/hegunhee/feature/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import com.hegunhee.feature.main.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,5 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewDataBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        // FIXME : 다크모드 정식 오픈시 해당 코드 제거
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
