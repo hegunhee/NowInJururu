9d62ed9f7a98e4595ca3b6a2fc1834c6eb4c128b
hegunhee <gunhee0072@gmail.com>
2023-05-25 19:42:28 +0900

    [stream-bottom-sheet] Streamer 삭제하는 UseCase 생성

---
+++ domain/src/main/java/com/hegunhee/domain/usecase/DeleteStreamerDataUseCase.kt
@@ -0,0 +1,13 @@
+package com.hegunhee.domain.usecase
+
+import com.hegunhee.domain.model.StreamerData
+import com.hegunhee.domain.repository.Repository
+import javax.inject.Inject
+
+class DeleteStreamerDataUseCase @Inject constructor(private val repository: Repository){
+
+    suspend operator fun invoke(streamerData: StreamerData) : Result<Unit>{
+        return repository.deleteStreamer(streamerData)
+    }
+    
+}
