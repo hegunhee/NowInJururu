a67edabc8985ff48e4bb48cf71ba7e0aec2444ab
hegunhee <gunhee0072@gmail.com>
2023-05-25 14:46:26 +0900

    [StreamBottomSheet] feat : RoomDB에서 스트리머를 삭제하는 코드 작성

--- data/src/main/java/com/hegunhee/data/database/dao/StreamerDao.kt
+++ data/src/main/java/com/hegunhee/data/database/dao/StreamerDao.kt
@@ -1,6 +1,7 @@
 package com.hegunhee.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -18,5 +19,8 @@ interface StreamerDao {
 
     @Query("SELECT * FROM StreamerEntity")
     suspend fun getAllStreamerEntityList() : List<StreamerEntity>
+
+    @Delete
+    suspend fun deleteStreamer(streamerEntity : StreamerEntity)
     
 }
