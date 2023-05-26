eeb0911ab8579560b3c86da63d81a367ace7ddd5
hegunhee <gunhee0072@gmail.com>
2023-05-26 16:13:10 +0900

    [stream-bottom-sheet] feat : StreamActionHandler에 더보기 버튼 클릭 함수 생성

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamActionHandler.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamActionHandler.kt
@@ -3,4 +3,6 @@ package com.hegunhee.feature.streamer
 interface StreamActionHandler {
 
     fun onClickTwitchStreamerItem(streamerLogin : String)
+
+    fun onClickMoreMenuButton(streamerLogin : String)
 }
