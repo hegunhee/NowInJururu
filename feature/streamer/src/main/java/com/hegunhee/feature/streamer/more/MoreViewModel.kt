f2a1063d9b1e13702720ca8d4b2126c0b6394e96
hegunhee <gunhee0072@gmail.com>
2023-05-26 17:54:04 +0900

    [stream-bottom-sheet] feat : 스트리머 이름을 저장하고있을 객체와 이름을 불러오는 코드 생성

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/more/MoreViewModel.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/more/MoreViewModel.kt
@@ -3,12 +3,20 @@ package com.hegunhee.feature.streamer.more
 import androidx.lifecycle.ViewModel
 import com.hegunhee.feature.streamer.MoreMenuActionHandler
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MoreViewModel @Inject constructor() : ViewModel(), MoreMenuActionHandler {
 
+    val streamerLogin : MutableStateFlow<String> = MutableStateFlow<String>("")
+    
+    fun fetchData(streamerName : String) {
+        streamerLogin.value = streamerName
+    }
+
     override fun onClickDeleteStreamerButton() {
-        
+
     }
 }
