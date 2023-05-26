b5e3f2aee4caef0b4c02337d06af5ac6f1236188
hegunhee <gunhee0072@gmail.com>
2023-05-26 21:17:53 +0900

    [stream-bottom-sheet] feat : 성공적으로 스트리머가 삭제되었을때 결과를 emit 하는 코드 작성

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/more/MoreViewModel.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/more/MoreViewModel.kt
@@ -6,8 +6,7 @@ import com.hegunhee.domain.model.StreamerData
 import com.hegunhee.domain.usecase.DeleteStreamerDataUseCase
 import com.hegunhee.feature.streamer.MoreMenuActionHandler
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,6 +17,9 @@ class MoreViewModel @Inject constructor(
 
     val streamerLogin : MutableStateFlow<String> = MutableStateFlow<String>("")
 
+    private val _navigateDismissDialog : MutableSharedFlow<Unit> = MutableSharedFlow<Unit>()
+    val navigateDismissDialog : SharedFlow<Unit> = _navigateDismissDialog.asSharedFlow()
+
     fun fetchData(streamerName : String) {
         streamerLogin.value = streamerName
     }
@@ -25,6 +27,11 @@ class MoreViewModel @Inject constructor(
     override fun onClickDeleteStreamerButton() {
         viewModelScope.launch {
             deleteStreamerDataUseCase(StreamerData(streamerLogin.value))
+                .onSuccess {
+                    _navigateDismissDialog.emit(Unit)
+                }.onFailure {
+
+                }
         }
     }
 }
