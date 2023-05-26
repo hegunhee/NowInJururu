ee000d244ca6bed79271071df51d50cf7f7634e8
hegunhee <gunhee0072@gmail.com>
2023-05-26 18:09:21 +0900

    [stream-bottom-sheet] feat : fragment에서 viewModel 연동과 argument 코드 작성

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/more/MoreBottomDialogFragment.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/more/MoreBottomDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.hegunhee.feature.streamer.R
 import com.hegunhee.feature.streamer.databinding.DialogMoreBinding
@@ -11,22 +12,25 @@ import com.hegunhee.feature.streamer.databinding.DialogMoreBinding
 class MoreBottomDialogFragment() : BottomSheetDialogFragment() {
 
     private lateinit var viewDataBinding : DialogMoreBinding
+    private val viewModel : MoreViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.dialog_more,container,false)
-        viewDataBinding = DialogMoreBinding.bind(root)
+        arguments?.let {
+            it.getString("streamerLogin")?.let { streamerName ->
+                viewModel.fetchData(streamerName)
+            }
+        }
+        viewDataBinding = DialogMoreBinding.bind(root).apply {
+            viewModel = this@MoreBottomDialogFragment.viewModel
+        }
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        arguments?.let {
-            it.getString("streamerLogin")?.let {
-                viewDataBinding.unfollowText.text = it
-            }
-        }
     }
 }
