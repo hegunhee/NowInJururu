70bd7ac913f369a5179459f992ce1ef01102ed18
hegunhee <gunhee0072@gmail.com>
2023-05-27 09:43:04 +0900

    fix : 빈 번들 객체를 만드는것이 아닌 내부에 만들어져있는 empty bundle 값 사용

--- feature/search/src/main/java/com/hegunhee/feature/search/SearchFragment.kt
+++ feature/search/src/main/java/com/hegunhee/feature/search/SearchFragment.kt
@@ -69,6 +69,6 @@ class SearchFragment : Fragment(){
     }
 
     private fun callRefreshStreamData() {
-        setFragmentResult(streamRequestKey, bundleOf())
+        setFragmentResult(streamRequestKey, Bundle.EMPTY)
     }
 }
