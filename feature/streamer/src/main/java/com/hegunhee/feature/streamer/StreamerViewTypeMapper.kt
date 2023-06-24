65b319f932735c118e5ba5afeacfea8b5360f4da
hegunhee <gunhee0072@gmail.com>
2023-06-24 14:54:06 +0900

    [streamer-recyclerview-header] feat : 도메인 레이어에서 받은 모델을 뷰타입 객체로 변경 매퍼함수

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerViewTypeMapper.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerViewTypeMapper.kt
@@ -2,6 +2,35 @@ package com.hegunhee.feature.streamer
 
 import com.hegunhee.domain.model.StreamDataType
 
+fun List<StreamDataType>.toStreamViewTypeData() : List<StreamerViewType> {
+    val groupDataType = this.groupBy { it is StreamDataType.StreamData }.values.toList()
+    val streamerViewTypeList = mutableListOf<StreamerViewType>()
+    if(groupDataType.isNotEmpty()){
+        if(groupDataType.size == 1){
+            when(groupDataType[0][0]) {
+                is StreamDataType.StreamData -> {
+                    streamerViewTypeList.add(StreamerViewType.LiveStreamerHeader(groupDataType[0].size))
+                    val liveStreamList = groupDataType[0].filterIsInstance<StreamDataType.StreamData>().map { it.toLiveStreamer() }
+                    streamerViewTypeList.addAll(liveStreamList)
+                }
+                is StreamDataType.EmptyData -> {
+                    streamerViewTypeList.add(StreamerViewType.UnLiveStreamerHeader(groupDataType[0].size))
+                    val unLiveStreamList = groupDataType[0].filterIsInstance<StreamDataType.EmptyData>().map { it.toUnLiveStreamer() }
+                    streamerViewTypeList.addAll(unLiveStreamList)
+                }
+            }
+        }else if(groupDataType.size == 2) {
+            streamerViewTypeList.add(StreamerViewType.LiveStreamerHeader(groupDataType[0].size))
+            val liveStreamList = groupDataType[0].filterIsInstance<StreamDataType.StreamData>().map { it.toLiveStreamer() }
+            streamerViewTypeList.addAll(liveStreamList)
+            streamerViewTypeList.add(StreamerViewType.UnLiveStreamerHeader(groupDataType[1].size))
+            val unLiveStreamList = groupDataType[1].filterIsInstance<StreamDataType.EmptyData>().map { it.toUnLiveStreamer() }
+            streamerViewTypeList.addAll(unLiveStreamList)
+        }
+    }
+    return streamerViewTypeList
+}
+
 fun StreamDataType.StreamData.toLiveStreamer() : StreamerViewType.LiveStreamer {
     return StreamerViewType.LiveStreamer(
         gameId = gameId,
