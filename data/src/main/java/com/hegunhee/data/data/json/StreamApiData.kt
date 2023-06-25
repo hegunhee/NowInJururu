df3d348ffd62aea124186c3e9d4de792a051690e
hegunhee <gunhee0072@gmail.com>
2023-06-25 17:56:32 +0900

    [variable-name] feat : StreamApiData Json 예시 주석 추가

--- data/src/main/java/com/hegunhee/data/data/json/StreamApiData.kt
+++ data/src/main/java/com/hegunhee/data/data/json/StreamApiData.kt
@@ -3,6 +3,29 @@ package com.hegunhee.data.data.json
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+/**
+ *  "id": "40398491671",
+    "user_id": "856575550",
+    "user_login": "ayatsunoyuni_stellive",
+    "user_name": "아야츠노_유니",
+    "game_id": "516575",
+    "game_name": "VALORANT",
+    "type": "live",
+    "title": "워치파티 !!!!",
+    "viewer_count": 8064,
+    "started_at": "2023-06-25T03:36:17Z",
+    "language": "ko",
+    "thumbnail_url": "https://static-cdn.jtvnw.net/previews-ttv/live_user_ayatsunoyuni_stellive-{width}x{height}.jpg",
+    "tag_ids": [],
+    "tags": [
+        "브이튜버",
+        "스텔라이브",
+        "유니",
+        "아야츠노유니",
+        "한국어"
+    ],
+    "is_mature": false
+ */
 @JsonClass(generateAdapter = true)
 data class StreamApiData(
     @Json(name = "game_id")val gameId: String,
