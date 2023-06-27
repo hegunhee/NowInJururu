7d13d2c02168e624c7d5490659e127bcde3a4a2e
hegunhee <gunhee0072@gmail.com>
2023-06-27 11:31:46 +0900

    [recommand-stream] test : domain module에서 사용할 model 데이터로 변환하는 테스트코드

--- data/src/test/java/com/hegunhee/data/TwitchGameStreamApiDataResponseUnitTest.kt
+++ data/src/test/java/com/hegunhee/data/TwitchGameStreamApiDataResponseUnitTest.kt
@@ -1,9 +1,9 @@
 package com.hegunhee.data
 
-import com.hegunhee.data.network.TwitchAuthTokenApi
-import com.hegunhee.data.network.TwitchAuthTokenBaseUrl
-import com.hegunhee.data.network.TwitchGetBaseUrl
-import com.hegunhee.data.network.TwitchStreamDataApi
+import com.hegunhee.data.mapper.toSearchDataList
+import com.hegunhee.data.mapper.toStreamData
+import com.hegunhee.data.network.*
+import com.hegunhee.domain.model.StreamDataType
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
@@ -12,12 +12,14 @@ class TwitchGameStreamApiDataResponseUnitTest {
 
     private lateinit var tokenApi : TwitchAuthTokenApi
     private lateinit var streamDataApi : TwitchStreamDataApi
+    private lateinit var streamerDataApi : TwitchStreamerDataApi
 
     @Before
     fun initMoshiAndRetrofit()  {
         val moshi = getMoshi()
         tokenApi = getRetrofit(moshi = moshi, baseUrl = TwitchAuthTokenBaseUrl).getTokenApi()
         streamDataApi = getRetrofit(moshi = moshi,baseUrl = TwitchGetBaseUrl).getStreamApi()
+        streamerDataApi = getRetrofit(moshi = moshi,baseUrl = TwitchGetBaseUrl).getStreamerDataApi()
     }
 
     /**
@@ -36,6 +38,27 @@ class TwitchGameStreamApiDataResponseUnitTest {
             }.onFailure {
                 println(it.toString())
                 assert(false)
+            }
+        }
+    }
+
+    @Test
+    fun `get gameStreamApiData to model data`() {
+        runBlocking {
+            val token = tokenApi.getAuthToken().getFormattedToken()
+            println("Bearer $token")
+            val gameStreamList = streamDataApi.getGameStreamData(authorization = token,gameId = "19976").streamApiData
+            if(gameStreamList.isEmpty()){
+                println("data is empty")
+                assert(true)
+            }else{
+                val streamerInfoList = streamerDataApi.getStreamerData(userLogin = gameStreamList.map { it.streamerId }.toTypedArray(), authorization = token).streamerApiDataList
+                gameStreamList.mapIndexed { index, streamApiData ->
+                    streamApiData.toStreamData(streamerInfoList[index].profileImageUrl)
+                }.forEach {
+                    println(it.toString())
+                }
+                assert(true)
             }
         }
     }
