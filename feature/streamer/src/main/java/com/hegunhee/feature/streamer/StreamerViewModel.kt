e18b10e1fb03d18616bfc53a97add39278d5996b
hegunhee <gunhee0072@gmail.com>
2023-06-28 14:49:57 +0900

    [recommand-stream] feat : 기존의 스트림 데이터중에 가장 많은 게임으로 해당 게임 스트림 데이터 추출

--- feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerViewModel.kt
+++ feature/streamer/src/main/java/com/hegunhee/feature/streamer/StreamerViewModel.kt
@@ -3,6 +3,7 @@ package com.hegunhee.feature.streamer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hegunhee.domain.model.StreamDataType
+import com.hegunhee.domain.model.StreamerData
 import com.hegunhee.domain.usecase.GetBookmarkedStreamDataListUseCase
 import com.hegunhee.domain.usecase.GetGameStreamDataListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,6 +20,9 @@ class StreamerViewModel @Inject constructor(
     private val _streamDataList : MutableStateFlow<List<StreamerViewType>> = MutableStateFlow(emptyList())
     val streamDataList : StateFlow<List<StreamerViewType>> = _streamDataList.asStateFlow()
 
+    private val _gameStreamDataList : MutableStateFlow<List<StreamerViewType.OnlineStreamer>> = MutableStateFlow(emptyList())
+    val gameStreamDataList : StateFlow<List<StreamerViewType.OnlineStreamer>> = _gameStreamDataList.asStateFlow()
+
     private val _navigateStreamerTwitch : MutableSharedFlow<String> = MutableSharedFlow()
     val navigateStreamerTwitch : SharedFlow<String> = _navigateStreamerTwitch.asSharedFlow()
 
@@ -27,11 +31,24 @@ class StreamerViewModel @Inject constructor(
 
     fun fetchBookmarkedStreamData() = viewModelScope.launch{
         getBookmarkedStreamDataListUseCase()
-            .onSuccess {
-                _streamDataList.emit(it.toStreamViewTypeData())
+            .onSuccess { streamDataList ->
+                _streamDataList.emit(streamDataList.toStreamViewTypeData())
+                fetchRecommandGameStreamData(streamDataList.filterIsInstance<StreamDataType.OnlineData>())
             }.onFailure {
 
             }
+    }
+
+    private suspend fun fetchRecommandGameStreamData(onlineStreamList : List<StreamDataType.OnlineData>) {
+        if(onlineStreamList.isNotEmpty()){
+            val gameId = onlineStreamList.map { it.gameId }.groupBy { it }.maxBy { it.value.size }.key
+            getGameStreamDataListUseCase(gameId)
+                .onSuccess {
+                    _gameStreamDataList.emit(it.map { it.toOnlineStreamer() })
+                }.onFailure {
+                    
+                }
+        }
     }
 
     override fun onClickTwitchStreamerItem(streamerLogin: String) {
