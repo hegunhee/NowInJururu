9737f9c913ceb8a22e5a39130397dfd24c86957b
hegunhee <gunhee0072@gmail.com>
2023-07-03 15:22:04 +0900

    [lottie-dialog] feat : LottieDialog가 검색할때 나오도록 설정

--- feature/search/src/main/java/com/hegunhee/feature/search/SearchFragment.kt
+++ feature/search/src/main/java/com/hegunhee/feature/search/SearchFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.hegunhee.feature.common.fragmentResultKeys.streamRequestKey
+import com.hegunhee.feature.common.lottie.LottieDialog
 import com.hegunhee.feature.common.twitch.handleTwitchDeepLink
 import com.hegunhee.feature.search.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,6 +22,7 @@ class SearchFragment : Fragment(){
     private lateinit var viewDataBinding : FragmentSearchBinding
     private val viewModel : SearchViewModel by viewModels()
     private lateinit var searchAdapter : SearchAdapter
+    private val lottieDialog = LottieDialog()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -62,6 +64,15 @@ class SearchFragment : Fragment(){
             launch {
                 viewModel.isBookMarkSuccess.collect{
                     callRefreshStreamData()
+                }
+            }
+            launch {
+                viewModel.isLoading.collect { isLoading ->
+                    if(isLoading){
+                        lottieDialog.show(parentFragmentManager,LottieDialog.TAG)
+                    }else{
+                        lottieDialog.dismissAllowingStateLoss()
+                    }
                 }
             }
         }
