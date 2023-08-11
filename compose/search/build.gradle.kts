4189992681c3dfbb0d2d2aba2e0dc6235fdd1e77
hegunhee <gunhee0072@gmail.com>
2023-08-11 16:08:34 +0900

    [compose-setting] build : compose-ui 모듈을 위한 plugin 생성

--- compose/search/build.gradle.kts
+++ compose/search/build.gradle.kts
@@ -1,54 +1,7 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("hegunhee.android.compose")
 }
 
 android {
     namespace = "com.hegunhee.compose.search"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.0"
-    }
-}
-
-dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(platform(libs.kotlin.bom))
-    implementation(libs.lifecycle.lifecycleScope)
-    implementation(libs.activity.compose)
-    implementation(platform(libs.compose.bom))
-    implementation(libs.bundles.compose.ui)
-    androidTestImplementation(platform(libs.compose.test.bom))
-    androidTestImplementation(libs.compose.ui.test.junit)
-    debugImplementation(libs.compose.ui.tooling)
-    debugImplementation(libs.compose.ui.test.manifest)
 }
---
+++ plugins/src/main/java/com/hegunhee/plugins/feature/AndroidComposePlugin.kt
@@ -0,0 +1,40 @@
+package com.hegunhee.plugins.feature
+
+import com.hegunhee.plugins.setup.setupAndroidCompose
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidComposePlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target){
+            with(pluginManager){
+                apply("hegunhee.android")
+                apply("hegunhee.android.hilt")
+            }
+            setupAndroidCompose()
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+            dependencies {
+                add("implementation",libs.findLibrary("core-ktx").get())
+                add("implementation",platform(libs.findLibrary("kotlin-bom").get()))
+                add("implementation",libs.findLibrary("lifecycle-lifecycleScope").get())
+                add("implementation",libs.findLibrary("activity-compose").get())
+                add("implementation",platform(libs.findLibrary("compose-bom").get()))
+                add("implementation",libs.findBundle("compose-ui").get())
+                add("implementation",libs.findLibrary("junit").get())
+                add("androidTestImplementation",libs.findLibrary("ext-junit").get())
+                add("androidTestImplementation",libs.findLibrary("espresso-core").get())
+
+                add("androidTestImplementation",platform(libs.findLibrary("compose-test-bom").get()))
+                add("androidTestImplementation",libs.findLibrary("compose-ui-test-junit").get())
+                add("debugImplementation",libs.findLibrary("compose-ui-tooling").get())
+                add("debugImplementation",libs.findLibrary("compose-ui-test-manifest").get())
+            }
+        }
+    }
+}
