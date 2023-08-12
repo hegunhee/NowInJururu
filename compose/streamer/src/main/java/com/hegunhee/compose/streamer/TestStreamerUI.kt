2f47f45a6654d3a6cff9a55e1c2dcb517b552fd5
hegunhee <gunhee0072@gmail.com>
2023-08-12 13:53:17 +0900

    [bottom-navigation] feat : Compose:streamer 모듈 추가

---
+++ compose/streamer/src/main/java/com/hegunhee/compose/streamer/TestStreamerUI.kt
@@ -0,0 +1,9 @@
+package com.hegunhee.compose.streamer
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TestStreamerUI() {
+    Text(text = "Hello Streamer")
+}
