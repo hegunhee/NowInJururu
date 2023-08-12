0f55b2f879b745eeccd16c6667d3517825ee3faa
hegunhee <gunhee0072@gmail.com>
2023-08-12 14:29:51 +0900

    [bottom-navigation] feat : 쓸데없는 Test용 Jururu UI 삭제

--- compose/jururu/src/main/java/com/hegunhee/compose/jururu/TestJururuUI.kt
+++
@@ -1,9 +0,0 @@
-package com.hegunhee.compose.jururu
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun TestJururuUI() {
-    Text(text = "Hello Jururu")
-}
