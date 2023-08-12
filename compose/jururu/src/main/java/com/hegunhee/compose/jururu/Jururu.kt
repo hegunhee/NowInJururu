4046612b4a08be69761bc5bbc5583ff64288ce15
hegunhee <gunhee0072@gmail.com>
2023-08-12 14:26:29 +0900

    [bottom-navigation] feat : Jururu NavGraph 작성 및 JururuScreenRoot 작성

---
+++ compose/jururu/src/main/java/com/hegunhee/compose/jururu/Jururu.kt
@@ -0,0 +1,11 @@
+package com.hegunhee.compose.jururu
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun JururuScreenRoot(
+
+) {
+    Text("Jururu Screen")
+}
