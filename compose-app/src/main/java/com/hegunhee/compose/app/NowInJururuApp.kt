7946796d9b493ad487f735eba317de573e7b2e55
hegunhee <gunhee0072@gmail.com>
2023-08-12 15:23:54 +0900

    [bottom-navigation] feat : App 전체를 관통하는 Scaffold 작성

--- compose-app/src/main/java/com/hegunhee/compose/app/NowInJururuApp.kt
+++ compose-app/src/main/java/com/hegunhee/compose/app/NowInJururuApp.kt
@@ -1,21 +1,36 @@
 package com.hegunhee.compose.app
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.hegunhee.compose.app.ui.theme.NowInJururuTheme
+import com.hegunhee.compose.jururu.JururuNavGraph
+import com.hegunhee.compose.jururu.jururuNavGraph
+import com.hegunhee.compose.search.searchNavGraph
+import com.hegunhee.compose.streamer.streamerNavGraph
 import kotlinx.coroutines.CoroutineScope
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NowInJururuApp(
     jururuAppScaffoldState : JururuAppScaffoldState = rememberJururuAppScaffoldState()
 ) {
     NowInJururuTheme() {
-        Text(text = "JururuApp")
+        Scaffold(bottomBar = { JururuBottomNavigation(backStackEntry = jururuAppScaffoldState.navController.currentBackStackEntryAsState(), onBottomClick = jururuAppScaffoldState::navigateBottomNavigation)}) { _ ->
+            NavHost(navController = jururuAppScaffoldState.navController, startDestination = JururuNavGraph.jururuRoute){
+                jururuNavGraph()
+
+                streamerNavGraph()
+
+                searchNavGraph()
+            }
+        }
     }
 }
 
@@ -36,6 +51,11 @@ class JururuAppScaffoldState(
     val coroutineScope : CoroutineScope,
     val navController: NavHostController,
 ) {
-
+    fun navigateBottomNavigation(index : Int) {
+        navController.navigate(bottomNavItems[index].screenRoute) {
+            launchSingleTop = true
+            restoreState = true
+        }
+    }
 }
 
