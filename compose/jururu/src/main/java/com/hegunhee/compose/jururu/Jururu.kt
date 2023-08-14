565ef2e259081c172cf55682c429015e468bb28b
hegunhee <gunhee0072@gmail.com>
2023-08-14 16:12:29 +0900

    [jururu-screen] feat : 상위 Scaffold의 paddingValues를 하위 컴포저블에 전달

--- compose/jururu/src/main/java/com/hegunhee/compose/jururu/Jururu.kt
+++ compose/jururu/src/main/java/com/hegunhee/compose/jururu/Jururu.kt
@@ -1,11 +1,23 @@
 package com.hegunhee.compose.jururu
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hegunhee.ui_component.text.ScreenHeaderText
 
 @Composable
 fun JururuScreenRoot(
 
 ) {
-    Text("Jururu Screen")
+    JururuScreen()
 }
+
+@Composable
+fun JururuScreen() {
+    Column(modifier = Modifier.fillMaxSize().padding(LocalPaddingValues.current)) {
+        ScreenHeaderText(text = "주르르")
+    }
+}
+
