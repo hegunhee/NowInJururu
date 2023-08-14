3cd48133c5e4183ba0ca9f18d05dc0f64db203f8
hegunhee <gunhee0072@gmail.com>
2023-08-14 17:46:54 +0900

    [jururu-screen] feat : JururuScreenRoot에서 UiState가 잘 받아오는지 테스트

--- compose/jururu/src/main/java/com/hegunhee/compose/jururu/Jururu.kt
+++ compose/jururu/src/main/java/com/hegunhee/compose/jururu/Jururu.kt
@@ -3,21 +3,36 @@ package com.hegunhee.compose.jururu
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.hegunhee.ui_component.text.ScreenHeaderText
 
 @Composable
 fun JururuScreenRoot(
-
+    viewModel : JururuViewModel = hiltViewModel()
 ) {
-    JururuScreen()
+    JururuScreen(uiState = viewModel.uiState.value)
 }
 
 @Composable
-fun JururuScreen() {
-    Column(modifier = Modifier.fillMaxSize().padding(LocalPaddingValues.current)) {
+fun JururuScreen(uiState : JururuUiState) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(LocalPaddingValues.current)) {
         ScreenHeaderText(text = "주르르")
+        when(uiState) {
+            is JururuUiState.Loading -> {
+                Text(text = uiState.toString())
+            }
+            is JururuUiState.Success -> {
+                Text(text = uiState.streamerList.toString())
+            }
+            is JururuUiState.Error -> {
+                Text(text = uiState.toString())
+            }
+        }
     }
 }
 
