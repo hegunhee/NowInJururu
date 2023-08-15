db738139e9589acecd46360d69d5a8703029c13a
hegunhee <gunhee0072@gmail.com>
2023-08-15 22:09:02 +0900

    [resource-common-module] feat : resource gradle에서 사용하지 않는 의존성 제거

--- resource_common/build.gradle.kts
+++ resource_common/build.gradle.kts
@@ -5,10 +5,3 @@ plugins {
 android {
     namespace = "com.hegunhee.resource_common"
 }
-
-dependencies {
-
-//    implementation("androidx.core:core-ktx:1.8.0")
-//    implementation("androidx.appcompat:appcompat:1.6.1")
-//    implementation("com.google.android.material:material:1.5.0")
-}
