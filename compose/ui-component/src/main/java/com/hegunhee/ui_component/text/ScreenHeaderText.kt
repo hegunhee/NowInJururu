3bf1e584e95ea91a2d985ad3d9d27c08b13fd74c
hegunhee <gunhee0072@gmail.com>
2023-08-16 20:50:26 +0900

    [jururu-screen] feat : header text 패딩값 dimens.xml에서 가져와 사용

--- compose/ui-component/src/main/java/com/hegunhee/ui_component/text/ScreenHeaderText.kt
+++ compose/ui-component/src/main/java/com/hegunhee/ui_component/text/ScreenHeaderText.kt
@@ -4,18 +4,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.hegunhee.resource_common.R
 
 @Composable
 fun ScreenHeaderText(text : String) {
     Text(text = text, fontSize = 40.sp, maxLines = 1,modifier = Modifier.padding(
-        start = 20.dp,
-        top = 10.dp,
-        bottom = 10.dp,
-        end = 20.dp
+        start = dimensionResource(id = R.dimen.header_start_padding),
+        top = dimensionResource(id = R.dimen.header_top_padding),
+        bottom = dimensionResource(id = R.dimen.header_bottom_padding),
+        end = dimensionResource(id = R.dimen.header_end_padding)
     ))
+
 }
 
 @Preview
