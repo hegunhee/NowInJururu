aa09b6d1f737e14e8b5a2d049e2e689e24b1129c
hegunhee <gunhee0072@gmail.com>
2023-08-18 16:56:17 +0900

    [jururu-screen] feat : Twitch 딥링크 이동을 담당하는 controller class 작성

--- compose-app/src/main/java/com/hegunhee/compose/app/NowInJururuApp.kt
+++ compose-app/src/main/java/com/hegunhee/compose/app/NowInJururuApp.kt
@@ -1,13 +1,16 @@
 package com.hegunhee.compose.app
 
-import androidx.compose.foundation.layout.PaddingValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -18,6 +21,7 @@ import com.hegunhee.compose.jururu.JururuNavGraph
 import com.hegunhee.compose.jururu.jururuNavGraph
 import com.hegunhee.compose.search.searchNavGraph
 import com.hegunhee.compose.streamer.streamerNavGraph
+import com.hegunhee.resource_common.R
 import kotlinx.coroutines.CoroutineScope
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -69,3 +73,32 @@ class JururuAppScaffoldState(
     }
 }
 
+@Composable
+fun rememberTwitchNavigationController() : TwitchNavigationController {
+    val context = LocalContext.current
+    return remember(context) {
+        TwitchNavigationController(context)
+    }
+}
+
+class TwitchNavigationController(
+    private val context : Context
+) {
+    fun navigate(url : String) {
+        val isTwitchAppInstalled = runCatching { isInstalledTwitchAppOrException() }.isSuccess
+        Intent().apply {
+            data = if(isTwitchAppInstalled) {
+                Uri.parse(url)
+            }else{
+                Uri.parse(context.getString(R.string.playStoreTwitchDeepLink))
+            }
+            context.startActivity(this)
+        }
+    }
+
+    private fun isInstalledTwitchAppOrException() : PackageInfo {
+        return context.run {
+            packageManager.getPackageInfo(getString(R.string.twitchPackageName), PackageManager.PackageInfoFlags.of(0L))
+        }
+    }
+}
