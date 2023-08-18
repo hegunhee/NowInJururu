a56ec96d0af4c61284d51807e1bcd87c88b4d806
hegunhee <gunhee0072@gmail.com>
2023-08-18 17:44:35 +0900

    [jururu-screen] feat : JururuUiState 상태별로 UI 작성

--- compose/jururu/src/main/java/com/hegunhee/compose/jururu/Jururu.kt
+++ compose/jururu/src/main/java/com/hegunhee/compose/jururu/Jururu.kt
@@ -1,15 +1,15 @@
 package com.hegunhee.compose.jururu
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.hegunhee.ui_component.item.OfflineStream
+import com.hegunhee.ui_component.item.OnlineStream
 import com.hegunhee.ui_component.text.ScreenHeaderText
 
 @Composable
@@ -28,30 +28,38 @@ fun JururuScreen(
     uiState : JururuUiState,
     onNavigateTwitchChannelClick : (String) -> Unit
 ) {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(LocalPaddingValues.current)) {
+    Column(modifier = Modifier.fillMaxSize().padding(LocalPaddingValues.current)) {
         ScreenHeaderText(text = "주르르")
-        val context = LocalContext.current
-        OfflineStream(
-            streamerId = "cotton__123",
-            streamerName = "주르르",
-            streamerProfileUrl = "https://static-cdn.jtvnw.net/jtv_user_pictures/919e1ba0-e13e-49ae-a660-181817e3970d-profile_image-300x300.png",
-            onTwitchStreamClick = onNavigateTwitchChannelClick,
-            onUnfollowButtonClick =  {streamerId -> Toast.makeText(context,"click unfollowButton",
-                Toast.LENGTH_SHORT).show()}
-        )
         when(uiState) {
-            is JururuUiState.Loading -> {
-                Text(text = uiState.toString())
-            }
+            is JururuUiState.Loading -> {}
             is JururuUiState.Success -> {
-                Text(text = uiState.streamerList.toString())
-
+                LazyColumn() {
+                    items(items = uiState.onlineStreamData, key = {it.streamerId}) { streamData ->
+                        OnlineStream(
+                            streamerId = streamData.streamerId,
+                            streamerName = streamData.streamerName,
+                            title = streamData.title,
+                            gameName = streamData.gameName,
+                            tags = streamData.tags,
+                            thumbNailUrl = streamData.thumbnailUrl,
+                            profileUrl = streamData.profileUrl,
+                            viewerCount = streamData.viewerCount,
+                            onTwitchStreamClick = onNavigateTwitchChannelClick,
+                            onUnfollowButtonClick = {}
+                        )
+                    }
+                    items(items = uiState.offlineStreamData, key = {it.streamerId}) { streamData ->
+                        OfflineStream(
+                            streamerId = streamData.streamerId,
+                            streamerName = streamData.streamerName,
+                            streamerProfileUrl = streamData.profileUrl,
+                            onTwitchStreamClick = onNavigateTwitchChannelClick,
+                            onUnfollowButtonClick = {}
+                        )
+                    }
+                }
             }
-            is JururuUiState.Error -> {
-                Text(text = uiState.toString())
-            }
+            is JururuUiState.Error -> {}
         }
     }
 }
