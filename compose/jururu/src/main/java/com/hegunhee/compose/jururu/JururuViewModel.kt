fea0dd71b5ebddf625508c14711f3a659e8172a8
hegunhee <gunhee0072@gmail.com>
2023-08-18 17:40:58 +0900

    [jururu-screen] feat : JururuUiState의 Success 클래스의 스트림 정보를 온라인 오프라인 분리

--- compose/jururu/src/main/java/com/hegunhee/compose/jururu/JururuViewModel.kt
+++ compose/jururu/src/main/java/com/hegunhee/compose/jururu/JururuViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hegunhee.domain.model.StreamDataType
 import com.hegunhee.domain.usecase.GetJururuStreamDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -25,7 +26,9 @@ class JururuViewModel @Inject constructor(private val getJururuStreamDataUseCase
         viewModelScope.launch {
             getJururuStreamDataUseCase()
                 .onSuccess {
-                    _uiState.value = JururuUiState.Success(listOf(it))
+                    val onlineStreamData = listOf(it).filterIsInstance(StreamDataType.OnlineData::class.java)
+                    val offlineStreamData = listOf(it).filterIsInstance(StreamDataType.OfflineData::class.java)
+                    _uiState.value = JururuUiState.Success(onlineStreamData, offlineStreamData)
                 }.onFailure {
                     _uiState.value = JururuUiState.Error
                 }
