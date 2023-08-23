ead4d3013dbbb8cc632b9e019f589297279e37a9
hegunhee <gunhee0072@gmail.com>
2023-08-23 16:07:33 +0900

    [streamer-screen] feat : UiModel과 ViewModel 작성 및 팔로우중인 방송정보 불러오는 코드 작성

---
+++ compose/streamer/src/main/java/com/hegunhee/compose/streamer/StreamerViewModel.kt
@@ -0,0 +1,54 @@
+package com.hegunhee.compose.streamer
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hegunhee.domain.model.StreamDataType
+import com.hegunhee.domain.usecase.GetBookmarkedStreamDataListUseCase
+import com.hegunhee.domain.usecase.GetGameStreamDataListUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StreamerViewModel @Inject constructor(
+    private val getBookmarkedStreamDataListUseCase: GetBookmarkedStreamDataListUseCase,
+    private val getGameStreamDataListUseCase: GetGameStreamDataListUseCase
+) : ViewModel(){
+
+    init {
+        fetchStreamerData()
+    }
+    private val _uiModel : MutableState<StreamerUiModel> = mutableStateOf(StreamerUiModel.Loading)
+    val uiModel : State<StreamerUiModel>
+        get() = _uiModel
+
+    private fun fetchStreamerData() = viewModelScope.launch {
+        getBookmarkedStreamDataListUseCase()
+            .onSuccess { streamDataList ->
+                val onlineStreamItem = streamDataList.filterIsInstance(StreamDataType.OnlineData::class.java).toOnlineStreamItem()
+                val offlineStreamItem = streamDataList.filterIsInstance(StreamDataType.OfflineData::class.java).toOfflineStreamItem()
+                if(onlineStreamItem.items.isNotEmpty()) {
+                    val mostFollowGameId = onlineStreamItem.items.getMostFollowedGameId()
+                    getGameStreamDataListUseCase(mostFollowGameId)
+                        .onSuccess { recommendStreamDataList ->
+                            val recommendStreamItem = recommendStreamDataList.toRecommendStreamItem(mostFollowGameId)
+                            _uiModel.value = StreamerUiModel.Success(listOf(onlineStreamItem, offlineStreamItem, recommendStreamItem))
+                        }.onFailure {
+                            _uiModel.value = StreamerUiModel.Error
+                        }
+                }else{
+                    _uiModel.value = StreamerUiModel.Success(listOf(onlineStreamItem, offlineStreamItem, emptyRecommendStreamItem()))
+                }
+            }.onFailure {
+                _uiModel.value = StreamerUiModel.Error
+            }
+    }
+
+    private fun List<StreamDataType.OnlineData>.getMostFollowedGameId() : String {
+        return this.map {it.gameId}.groupBy { it }.maxBy { it.value.size }.key
+    }
+
+}
