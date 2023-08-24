fc742400c0df7ad331283f29ce195249f8eb2f7b
hegunhee <gunhee0072@gmail.com>
2023-08-24 15:20:06 +0900

    [streamer-screen] feat : compose 바텀시트 외부 라이브러리 추가 및 스트리머 바텀dialog 구현

--- compose/streamer/build.gradle.kts
+++ compose/streamer/build.gradle.kts
@@ -10,4 +10,6 @@ dependencies{
     implementation(project(":domain"))
     implementation(project(":compose:ui-component"))
     implementation(project(":resource_common"))
+
+    implementation(libs.compose.bottomsheetdialog)
 }
