15d1693f14f0a26fd9cd9c0928b66275a4346c93
hegunhee <gunhee0072@gmail.com>
2023-08-24 15:28:11 +0900

    [streamer-screen] feat : viewModel에서 팔로우 취소 이벤트 작성 및 적용

--- compose/streamer/src/main/java/com/hegunhee/compose/streamer/StreamerViewModel.kt
+++ compose/streamer/src/main/java/com/hegunhee/compose/streamer/StreamerViewModel.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hegunhee.domain.model.StreamDataType
+import com.hegunhee.domain.model.StreamerData
+import com.hegunhee.domain.usecase.DeleteStreamerDataUseCase
 import com.hegunhee.domain.usecase.GetBookmarkedStreamDataListUseCase
 import com.hegunhee.domain.usecase.GetGameStreamDataListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,7 +19,8 @@ import javax.inject.Inject
 @HiltViewModel
 class StreamerViewModel @Inject constructor(
     private val getBookmarkedStreamDataListUseCase: GetBookmarkedStreamDataListUseCase,
-    private val getGameStreamDataListUseCase: GetGameStreamDataListUseCase
+    private val getGameStreamDataListUseCase: GetGameStreamDataListUseCase,
+    private val deleteStreamerDataUseCase: DeleteStreamerDataUseCase
 ) : ViewModel(){
 
     init {
@@ -51,6 +54,18 @@ class StreamerViewModel @Inject constructor(
 
     private fun List<StreamDataType.OnlineData>.getMostFollowedGameId() : String {
         return this.map {it.gameId}.groupBy { it }.maxBy { it.value.size }.key
+    }
+
+    fun onUnfollowStreamerClick(streamerId : String) {
+        viewModelScope.launch {
+            deleteStreamerDataUseCase(StreamerData(streamerId))
+                .onSuccess {
+                    fetchStreamerData()
+                }.onFailure {
+                    _uiModel.value = StreamerUiModel.Error
+                }
+        }
+
     }
 
 }
