5490a56ccb934df344a5cc73723e49805410f3ab
hegunhee <gunhee0072@gmail.com>
2023-08-25 14:55:26 +0900

    [common-design] style : streamer 화면 하드코딩된 padding, size 들을 파일에 가져와서 사용

--- compose/ui-component/src/main/java/com/hegunhee/ui_component/style/FontSize.kt
+++ compose/ui-component/src/main/java/com/hegunhee/ui_component/style/FontSize.kt
@@ -3,6 +3,7 @@ package com.hegunhee.ui_component.style
 import androidx.compose.ui.unit.sp
 
 val headerTextFontSize = 40.sp
+val largeTextFontSize = 25.sp
 val middleTextFontSize = 15.sp
 
 val middleButtonFontSize = 13.sp
