97d4363446af8dad63f3fdf3b82bde262716a05f
hegunhee <gunhee0072@gmail.com>
2023-08-25 14:24:19 +0900

    [common-design] style : search UI에서 하드코딩된 padding size값 xml로 추출

--- compose/ui-component/src/main/java/com/hegunhee/ui_component/item/SearchStreamer.kt
+++ compose/ui-component/src/main/java/com/hegunhee/ui_component/item/SearchStreamer.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -26,14 +27,17 @@ fun SearchStreamer(
     onFollowButtonClick : (String) -> Unit
 ) {
     val context = LocalContext.current
-    Row(modifier = Modifier.fillMaxWidth().clickable { onItemClick(String.format(context.getString(R.string.twitchChannelUrl),streamerId)) }) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .clickable { onItemClick(String.format(context.getString(R.string.twitchChannelUrl),streamerId)) }
+        .padding(horizontal = dimensionResource(R.dimen.header_start_padding))) {
         AsyncImage(
             model = profileUrl,
             contentDescription = "$streamerName 입니다.",
-            modifier = Modifier.size(100.dp).padding(vertical = 10.dp).alignByBaseline()
+            modifier = Modifier.size(dimensionResource(R.dimen.search_profile_image_height_size)).padding(vertical = dimensionResource(R.dimen.search_profile_image_vertical_padding)).alignByBaseline()
         )
         Column(
-            modifier = Modifier.padding(start = 10.dp).align(Alignment.Bottom),
+            modifier = Modifier.padding(start = dimensionResource(R.dimen.item_between_start_margin)).align(Alignment.Bottom),
         ) {
             Text(text = streamerName,fontSize = 15.sp)
             Text(text = streamerId, fontSize = 15.sp)
@@ -42,5 +46,4 @@ fun SearchStreamer(
             }
         }
     }
-
 }
