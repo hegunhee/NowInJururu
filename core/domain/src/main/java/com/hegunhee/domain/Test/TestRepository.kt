6d989588a73ebafaf4b1eb53a7a66ea83cf9269e
hegunhee <gunhee0072@gmail.com>
2023-10-26 17:16:00 +0900

    refactor : 사용하지않는 Test용 클래스파일 삭제

--- core/domain/src/main/java/com/hegunhee/domain/Test/TestRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.hegunhee.domain.Test
-
-import com.hegunhee.domain.model.StreamDataType
-
-interface TestRepository {
-
-    suspend fun getStreamData(userLogin: String = "cotton__123") : Result<StreamDataType>
-}
