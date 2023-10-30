298e8f8dd739bc0acae90d350c88bec8cf48db44
hegunhee <gunhee0072@gmail.com>
2023-10-30 17:17:59 +0900

    [jururu-ui-v2] feat : ApiResult에 예시 추가

--- core/data/src/main/java/com/hegunhee/data/data/json/kakao/KakaoVideoSearchData.kt
+++ core/data/src/main/java/com/hegunhee/data/data/json/kakao/KakaoVideoSearchData.kt
@@ -3,6 +3,14 @@ package com.hegunhee.data.data.json.kakao
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+/**
+ * "author": "봉인 풀린 주르르",
+ * "datetime": "2023-10-27T15:00:31.000+09:00",
+ * "play_time": 103,
+ * "thumbnail": "https://search4.kakaocdn.net/argon/138x78_80_pr/7fDZAJLFEOB",
+ * "title": "주르르식 낭만",
+ * "url": "http://www.youtube.com/watch?v=v4xx2vFK2ts"
+ */
 @JsonClass(generateAdapter = true)
 data class KakaoVideoSearchData(
     @Json(name = "author") val author: String,
