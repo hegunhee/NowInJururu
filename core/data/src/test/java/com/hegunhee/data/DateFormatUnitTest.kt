871bc39acaa26e120b5398f8c141276476ddb55e
hegunhee <gunhee0072@gmail.com>
2023-10-31 16:08:41 +0900

    [jururu-ui-v2] test : String타입의 날짜정보를 Date객체로 변경 후 다른 포맷으로 변경 테스트

---
+++ core/data/src/test/java/com/hegunhee/data/DateFormatUnitTest.kt
@@ -0,0 +1,25 @@
+package com.hegunhee.data
+
+import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class DateFormatUnitTest {
+
+    @Test
+    fun dateStringFormat() {
+        val dateTime = "2022-03-01T01:33:27.000+09:00"
+        runCatching {
+            val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+            val transferFormat = SimpleDateFormat("yyyy년 MM월 dd일 HH:mm")
+            val date = format.parse(dateTime)
+            transferFormat.format(date)
+        }.onSuccess {
+            println(it)
+            assert(true)
+        }.onFailure {
+            println(it.message)
+            assert(false)
+        }
+    }
+}
