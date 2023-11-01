fb03aa2a75118277f25775af7092d92f35bdd73a
hegunhee <gunhee0072@gmail.com>
2023-11-01 21:11:12 +0900

    [jururu-ui-v2] feat : 카카오 검색 viewHolder view xml, Adapter 코드 작성

---
+++ feature/searchkakao/src/main/java/com/hegunhee/nowinjururu/feature/searchkakao/KakaoSearchAdapter.kt
@@ -0,0 +1,47 @@
+package com.hegunhee.nowinjururu.feature.searchkakao
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.hegunhee.domain.model.kakao.KakaoSearchData
+import com.hegunhee.nowinjururu.feature.searchkakao.databinding.ItemSearchImageBinding
+import com.hegunhee.nowinjururu.feature.searchkakao.databinding.ItemSearchVideoBinding
+import com.hegunhee.nowinjururu.feature.searchkakao.databinding.ItemSearchWebBinding
+import kotlin.IllegalArgumentException
+
+class KakaoSearchAdapter : ListAdapter<KakaoSearchData,KakaoSearchViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KakaoSearchViewHolder {
+        return when(viewType) {
+            R.layout.item_search_web -> {KakaoSearchWebViewHolder(ItemSearchWebBinding.inflate(LayoutInflater.from(parent.context),parent,false)) }
+            R.layout.item_search_image -> { KakaoSearchImageViewHolder(ItemSearchImageBinding.inflate(LayoutInflater.from(parent.context),parent,false))}
+            R.layout.item_search_video -> { KakaoSearchVideoViewHolder(ItemSearchVideoBinding.inflate(LayoutInflater.from(parent.context),parent,false)) }
+            else -> { throw IllegalArgumentException("") }
+        }
+    }
+
+    override fun onBindViewHolder(holder: KakaoSearchViewHolder, position: Int) {
+        holder.bindView(getItem(position))
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when(getItem(position)){
+            is KakaoSearchData.Web -> R.layout.item_search_web
+            is KakaoSearchData.Image -> R.layout.item_search_image
+            is KakaoSearchData.Video -> R.layout.item_search_video
+        }
+    }
+    companion object {
+        private object diffUtil : DiffUtil.ItemCallback<KakaoSearchData>() {
+            override fun areItemsTheSame(oldItem: KakaoSearchData, newItem: KakaoSearchData): Boolean {
+                return oldItem.type.name + oldItem.time == newItem.type.name + newItem.time
+            }
+
+            override fun areContentsTheSame(oldItem: KakaoSearchData, newItem: KakaoSearchData): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
---
+++ feature/searchkakao/src/main/java/com/hegunhee/nowinjururu/feature/searchkakao/KakaoSearchViewHolder.kt
@@ -0,0 +1,40 @@
+package com.hegunhee.nowinjururu.feature.searchkakao
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.hegunhee.domain.model.kakao.KakaoSearchData
+import com.hegunhee.nowinjururu.feature.searchkakao.databinding.ItemSearchImageBinding
+import com.hegunhee.nowinjururu.feature.searchkakao.databinding.ItemSearchVideoBinding
+import com.hegunhee.nowinjururu.feature.searchkakao.databinding.ItemSearchWebBinding
+
+
+abstract class KakaoSearchViewHolder(private val itemView : View) : ViewHolder(itemView) {
+    abstract fun bindView(searchData : KakaoSearchData)
+
+}
+
+class KakaoSearchWebViewHolder(private val binding : ItemSearchWebBinding) : KakaoSearchViewHolder(binding.root) {
+
+    override fun bindView(searchData: KakaoSearchData) {
+        (searchData as? KakaoSearchData.Web)?.let {  webData ->
+            binding.title.text = webData.title
+        }
+    }
+}
+
+class KakaoSearchVideoViewHolder(private val binding : ItemSearchVideoBinding) : KakaoSearchViewHolder(binding.root) {
+
+    override fun bindView(searchData: KakaoSearchData) {
+        (searchData as? KakaoSearchData.Video)?.let { videoData ->
+            binding.title.text = videoData.title
+        }
+    }
+}
+
+class KakaoSearchImageViewHolder(private val binding : ItemSearchImageBinding) : KakaoSearchViewHolder(binding.root) {
+    override fun bindView(searchData: KakaoSearchData) {
+        (searchData as? KakaoSearchData.Image)?.let { imageData ->
+            binding.title.text = imageData.displaySiteName
+        }
+    }
+}
