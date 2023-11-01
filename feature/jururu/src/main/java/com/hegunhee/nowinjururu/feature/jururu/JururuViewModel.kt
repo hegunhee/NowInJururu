fd3e2a8fb7a69f153c3d1d3d2e0a188d99ecf7ac
hegunhee <gunhee0072@gmail.com>
2023-11-01 21:11:40 +0900

    [jururu-ui-v2] feat : kakao 검색결과 Adapter 연동

--- feature/jururu/src/main/java/com/hegunhee/nowinjururu/feature/jururu/JururuViewModel.kt
+++ feature/jururu/src/main/java/com/hegunhee/nowinjururu/feature/jururu/JururuViewModel.kt
@@ -2,8 +2,11 @@ package com.hegunhee.nowinjururu.feature.jururu
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hegunhee.domain.model.kakao.KakaoSearchData
+import com.hegunhee.domain.model.kakao.KakaoSearchSortType
 import com.hegunhee.domain.model.twitch.StreamDataType
 import com.hegunhee.domain.usecase.GetStreamDataUseCase
+import com.hegunhee.domain.usecase.GetWebSearchDataListUseCase
 import com.hegunhee.nowinjururu.core.navigation.twitch.TwitchDeepLink
 import com.hegunhee.nowinjururu.core.designsystem.adapter.streamer.StreamActionHandler
 import com.hegunhee.nowinjururu.core.designsystem.adapter.streamer.StreamerViewType
@@ -15,7 +18,10 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class JururuViewModel @Inject constructor(private val getStreamDataUseCase: GetStreamDataUseCase) : ViewModel(),
+class JururuViewModel @Inject constructor(
+    private val getStreamDataUseCase: GetStreamDataUseCase,
+    private val getWebSearchDataListUseCase : GetWebSearchDataListUseCase
+) : ViewModel(),
     StreamActionHandler {
 
     private val _favoriteStreamData : MutableStateFlow<StreamerViewType> = MutableStateFlow(StreamerViewType.OfflineEmpty)
@@ -24,6 +30,8 @@ class JururuViewModel @Inject constructor(private val getStreamDataUseCase: GetS
     private val _navigateTwitchDeepLink : MutableSharedFlow<TwitchDeepLink> = MutableSharedFlow()
     val navigateTwitchDeepLink : SharedFlow<TwitchDeepLink> = _navigateTwitchDeepLink.asSharedFlow()
 
+    private val _webSearchData : MutableStateFlow<List<KakaoSearchData.Web>> = MutableStateFlow(emptyList())
+    val webSearchData : StateFlow<List<KakaoSearchData.Web>> = _webSearchData.asStateFlow()
     fun getStreamData() {
         viewModelScope.launch {
             getStreamDataUseCase("cotton__123")
@@ -36,6 +44,17 @@ class JururuViewModel @Inject constructor(private val getStreamDataUseCase: GetS
 
                 }
                 .onFailure {  }
+        }
+    }
+
+    fun getWebSearchData() {
+        viewModelScope.launch {
+            getWebSearchDataListUseCase("주르르",KakaoSearchSortType.recency)
+                .onSuccess {
+                    _webSearchData.value = it
+                }.onFailure {
+
+                }
         }
     }
 
