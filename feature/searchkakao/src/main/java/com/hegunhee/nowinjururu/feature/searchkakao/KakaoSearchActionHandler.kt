f6f2240881d3dcf495ae6e82f47c3b0e34d311d6
hegunhee <gunhee0072@gmail.com>
2023-11-13 15:25:01 +0900

    [jururu-ui-v2] feat : 공유버튼 구현, 공유버튼 클릭 이벤트 함수 작성

--- feature/searchkakao/src/main/java/com/hegunhee/nowinjururu/feature/searchkakao/KakaoSearchActionHandler.kt
+++ feature/searchkakao/src/main/java/com/hegunhee/nowinjururu/feature/searchkakao/KakaoSearchActionHandler.kt
@@ -2,5 +2,7 @@ package com.hegunhee.nowinjururu.feature.searchkakao
 
 interface KakaoSearchActionHandler {
 
-    fun onSearchItemClick(url : String)
+    fun onSearchItemClick(url: String)
+
+    fun onShareButtonClick(url: String, title: String)
 }
