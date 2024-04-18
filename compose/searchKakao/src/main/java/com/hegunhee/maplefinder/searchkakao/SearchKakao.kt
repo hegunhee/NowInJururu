d468c1a0e49f9a17b9eee50445c4e96d4f7600e2
hegunhee <gunhee0072@gmail.com>
2024-04-18 20:28:00 +0900

    [compose-search-kakao] feat : 주르르 화면 헤더와 주르르 인터넷 검색결과 정렬 타입 작성

--- compose/searchKakao/src/main/java/com/hegunhee/maplefinder/searchkakao/SearchKakao.kt
+++ compose/searchKakao/src/main/java/com/hegunhee/maplefinder/searchkakao/SearchKakao.kt
@@ -1,19 +1,56 @@
 package com.hegunhee.maplefinder.searchkakao
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hegunhee.domain.model.kakao.KakaoSearchSortType
+import com.hegunhee.ui_component.text.ScreenHeaderText
 
 @Composable
 fun SearchKakaoScreenRoot(
     paddingValues: PaddingValues
 ) {
-    SearchKakaoScreen()
+    SearchKakaoScreen(
+        paddingValues,
+        onSearchSortTypeClick = { }
+    )
 }
 
 @Composable
 private fun SearchKakaoScreen(
-    
+    paddingValues: PaddingValues,
+    onSearchSortTypeClick: (KakaoSearchSortType) -> Unit
 ) {
-    Text(text = "카카오 검색창")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+    ) {
+        ScreenHeaderText(text = "주르르")
+        SearchTypeButtons(onSearchSortTypeClick = onSearchSortTypeClick)
+    }
+}
+
+@Composable
+private fun SearchTypeButtons(
+    onSearchSortTypeClick : (KakaoSearchSortType) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Button(onClick = { onSearchSortTypeClick(KakaoSearchSortType.Accuracy) }) {
+            Text("기본 검색")
+        }
+        Button(onClick = { onSearchSortTypeClick(KakaoSearchSortType.Recency)}) {
+            Text("최신순")
+        }
+    }
 }
