3a50239f85c03baf001515159010caac66b9a6f0
이건희 <leech9876@naver.com>
2024-11-28 22:09:06 +0900

    #64 - feat : Util 테스트코드 개선
    given_when_then 으로 나누고 assert 대신 assertEquals를 사용함

---
+++ core/data/src/test/java/com/hegunhee/data/utilTest/DateFormatChangeTest.kt
@@ -0,0 +1,25 @@
+package com.hegunhee.data.utilTest
+
+import org.junit.Assert
+import org.junit.Test
+import java.text.SimpleDateFormat
+
+class DateFormatChangeTest {
+
+    @Test
+    fun givenDate_whenChangeFormat_thenReturnChangedDate() {
+        // Given
+        val dateString = "2022-03-01T01:33:27.000+09:00"
+        val formattedDateString = "2022년 03월 01일 01:33"
+        val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+        val transferFormat = SimpleDateFormat("yyyy년 MM월 dd일 HH:mm")
+
+        // When
+        val changeFormatDate = format.parse(dateString) ?: NullPointerException()
+        val changeFormatDateString = transferFormat.format(changeFormatDate)
+
+        // Then
+        Assert.assertEquals(changeFormatDateString,formattedDateString)
+
+    }
+}
---
+++ core/data/src/test/java/com/hegunhee/data/utilTest/UrlDomainSplitTest.kt
@@ -0,0 +1,22 @@
+package com.hegunhee.data.utilTest
+
+import org.junit.Assert
+import org.junit.Test
+import java.net.URL
+
+class UrlDomainSplitTest {
+
+
+    @Test
+    fun givenUrl_WhenGetDomainName_ThenReturnDomainName() {
+        // Given
+        val domainName = "gameinsight.co.kr"
+        val url = "http://www.gameinsight.co.kr/news/articleView.html?idxno=30985"
+
+        // When
+        val urlDomain = URL(url).host.removePrefix("www.")
+
+        // Then
+        Assert.assertEquals(urlDomain,domainName)
+    }
+}
