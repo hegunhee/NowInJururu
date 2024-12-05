5e5378b251a40e2aaa30205d754737fca9a46d35
이건희 <leech9876@naver.com>
2024-12-05 22:01:00 +0900

    #70 - test : 카카오 페이징 테스트 작성

---
+++ core/data/src/test/java/com/hegunhee/data/repository/kakao/SearchPagingDataTest.kt
@@ -0,0 +1,70 @@
+package com.hegunhee.data.repository.kakao
+
+import androidx.paging.PagingData
+import com.hegunhee.data.dataSource.local.LocalDataSource
+import com.hegunhee.data.dataSource.remote.RemoteDataSource
+import com.hegunhee.data.repository.DefaultRepository
+import com.hegunhee.domain.model.kakao.KakaoSearchData
+import com.hegunhee.domain.model.kakao.KakaoSearchSortType
+import com.hegunhee.domain.model.kakao.KakaoSearchType
+import com.hegunhee.domain.model.twitch.SearchData
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class SearchPagingDataTest {
+
+    @InjectMocks
+    private lateinit var sut: DefaultRepository
+
+    @Mock
+    private lateinit var remoteDataSource: RemoteDataSource
+
+    @Mock
+    private lateinit var localDataSource: LocalDataSource
+
+    @Test
+    fun givenPagerInfo_whenSearchPagingData_thenReturnPagingData() {
+        runBlocking {
+            // Given
+            val query = "주르르"
+            val sortType = KakaoSearchSortType.Accuracy
+            val searchType = KakaoSearchType.Web
+            val size = 20
+            whenever(
+                remoteDataSource.getKakaoSearchPagingData(
+                    query,
+                    sortType,
+                    searchType,
+                    size
+                )
+            ).thenReturn(flowOf(createPagingData(query)))
+
+            // When
+            val pagingData = sut.getKakaoSearchPagingData(query, sortType, searchType, size)
+
+            // Then
+            assertTrue(pagingData.count() == 1)
+            verify(remoteDataSource).getKakaoSearchPagingData(query, sortType, searchType, size)
+        }
+    }
+
+    private fun createPagingData(streamerName: String): PagingData<KakaoSearchData> {
+        return PagingData.from(listOf(createSearchWebData(streamerName)))
+    }
+
+    private fun createSearchWebData(streamerName: String): KakaoSearchData {
+        return KakaoSearchData.Web("", "", streamerName, "")
+    }
+
+}
