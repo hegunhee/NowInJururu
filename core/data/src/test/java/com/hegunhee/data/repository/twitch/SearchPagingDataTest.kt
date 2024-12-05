4bf2ea96c699badd422fdeb67b12f4e8328d05c8
이건희 <leech9876@naver.com>
2024-12-05 21:48:21 +0900

    #70 - test : 트위치 검색 페이징 테스트코드 작성

---
+++ core/data/src/test/java/com/hegunhee/data/repository/twitch/SearchPagingDataTest.kt
@@ -0,0 +1,59 @@
+package com.hegunhee.data.repository.twitch
+
+import androidx.paging.PagingData
+import com.hegunhee.data.dataSource.local.LocalDataSource
+import com.hegunhee.data.dataSource.remote.RemoteDataSource
+import com.hegunhee.data.repository.DefaultRepository
+import com.hegunhee.domain.model.twitch.SearchData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class SearchPagingDataTest {
+
+    @InjectMocks
+    private lateinit var sut: DefaultRepository
+
+    @Mock
+    private lateinit var remoteDataSource: RemoteDataSource
+
+    @Mock
+    private lateinit var localDataSource: LocalDataSource
+
+    @Test
+    fun givenStreamerIdAndPageSize_whenSearchData_thenReturnPagingData() {
+        runBlocking {
+            // Given
+            val streamerName = "주르르"
+            val size = 20
+            whenever(remoteDataSource.getSearchPagingDataResponse(streamerName,size)).thenReturn(flowOf(createPagingData(streamerName)))
+            whenever(localDataSource.getAllStreamerList()).thenReturn(emptyList())
+
+            // When
+            val pagingData = sut.searchPagingSource(streamerName,size)
+
+            // Then
+            Assert.assertTrue(pagingData.count() == 1)
+            verify(remoteDataSource).getSearchPagingDataResponse(streamerName,size)
+            verify(localDataSource).getAllStreamerList()
+        }
+    }
+
+    private fun createPagingData(streamerName: String): PagingData<SearchData> {
+        return PagingData.from(listOf(createSearchData(streamerName)))
+    }
+
+    private fun createSearchData(streamerName: String): SearchData {
+        return SearchData("",streamerName,"",false, emptyList(),"","")
+    }
+}
