8380bdd3b92b76201b1409cdc82eba211d094ed1
이건희 <57277631+hegunhee@users.noreply.github.com>
2024-12-06 15:04:14 +0900

    Ui Test 작성 (#75)
    * #74 - test : 로딩중, 에러 화면 테스트
    
    해당 테스트는 추후 리팩토링시 ui-component 모듈로 이동 예정
    물론 로딩중, 에러 화면은 ui-component 모듈로 이동 예정
    
    * #74 - streamer 성공시 화면 테스트
    
    온라인, 오프라인, 게임추천별로 나눠서 단위 테스트
    
    * #74 - 검색 페이지 화면 테스트

---
+++ compose/streamer/src/androidTest/java/com/hegunhee/compose/streamer/SuccessItemsTest.kt
@@ -0,0 +1,80 @@
+package com.hegunhee.compose.streamer
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.hegunhee.domain.model.twitch.StreamDataType
+import org.junit.Rule
+import org.junit.Test
+
+class SuccessItemsTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun givenOnlineItem_whenStreamerScreen_showOnlineTextStreamerName() {
+        val streamerName = "주르르"
+        composeTestRule.setContent {
+            StreamerScreen(uiModel = StreamerUiModel.Success(listOf(createOnlineStreamItem(streamerName))),{},{},{})
+        }
+
+        composeTestRule
+            .onNodeWithText("온라인")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText(streamerName)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun givenOfflineItem_whenStreamerScreen_showOfflineTextAndStreamerName() {
+        val streamerName = "주르르"
+        composeTestRule.setContent {
+            StreamerScreen(uiModel = StreamerUiModel.Success(listOf(createOfflineStreamItem(streamerName))),{},{},{})
+        }
+
+        composeTestRule
+            .onNodeWithText("오프라인")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText(streamerName)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun givenGameStream_whenStreamerScreen_showRecommendTextAndGameName() {
+        val gameName = "메이플스토리"
+        composeTestRule.setContent {
+            StreamerScreen(uiModel = StreamerUiModel.Success(listOf(createGameStreamItem(gameName))),{},{},{})
+        }
+
+        composeTestRule
+            .onNodeWithText("추천 생방송 채널")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText(gameName)
+            .assertIsDisplayed()
+    }
+
+    private fun createOnlineStreamItem(streamerName: String):StreamItem {
+        return StreamItem.Online(listOf(createOnlineStreamData(streamerName = streamerName)))
+    }
+
+    private fun createOfflineStreamItem(streamerName: String): StreamItem {
+        return StreamItem.Offline(listOf(StreamDataType.OfflineData("",streamerName,"")))
+    }
+
+    private fun createGameStreamItem(gameName: String): StreamItem {
+        return StreamItem.Recommend(gameName,listOf(createOnlineStreamData(gameName = gameName)))
+    }
+
+    private fun createOnlineStreamData(streamerName: String = "", gameName: String = ""): StreamDataType.OnlineData {
+        return StreamDataType.OnlineData("",gameName,"",false,"","", emptyList(),"","","","","","",streamerName,"")
+    }
+
+}
