ca175944322fae763b2fb9bd067f60dc8ff48640
이건희 <leech9876@naver.com>
2024-12-06 14:42:53 +0900

    #74 - 검색 페이지 화면 테스트

---
+++ compose/search/src/androidTest/java/com/hegunhee/compose/search/SearchStreamerTest.kt
@@ -0,0 +1,53 @@
+package com.hegunhee.compose.search
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.hegunhee.domain.model.twitch.SearchData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import org.junit.Rule
+import org.junit.Test
+
+class SearchStreamerTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun givenSearchItem_whenStreamerScreen_showSearchStreamerName() {
+        val query = "주르르"
+        val streamerId = "cotton__123"
+        composeTestRule.setContent {
+            val pagingItems = createPagingData(streamerId,query).collectAsLazyPagingItems()
+            SearchScreen(uiModel = SearchUiModel.Success, searchQuery = query, searchResult = pagingItems,{},{},{},{})
+        }
+
+        composeTestRule
+            .onNodeWithText(streamerId)
+            .assertIsDisplayed()
+
+    }
+
+    private fun createPagingData(streamerId: String,query: String): Flow<PagingData<SearchData>> {
+        return flowOf(
+            PagingData.from(
+                listOf(
+                    SearchData(
+                        streamerId,
+                        query,
+                        "",
+                        false,
+                        emptyList(),
+                        "",
+                        ""
+                    )
+                )
+            )
+        )
+    }
+
+}
