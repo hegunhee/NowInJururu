d9b3d44932e8a675ab720b41821e6d8c82974896
이건희 <leech9876@naver.com>
2024-12-07 22:39:00 +0900

    #78 - style : 하단 바의 이름을 주르르에서 카카오 검색으로 변경

--- compose-app/src/main/java/com/hegunhee/compose/app/BottomNavItem.kt
+++ compose-app/src/main/java/com/hegunhee/compose/app/BottomNavItem.kt
@@ -7,7 +7,7 @@ import com.hegunhee.maplefinder.searchkakao.navigation.SEARCH_KAKAO_ROUTE
 sealed class BottomNavItem(
     val title : Int,val icon : Int, val screenRoute : String
 ) {
-    object Jururu : BottomNavItem(com.hegunhee.ui_component.R.string.jururu, com.hegunhee.ui_component.R.drawable.ic_star_24, SEARCH_KAKAO_ROUTE)
+    object Jururu : BottomNavItem(com.hegunhee.ui_component.R.string.kakao_search, com.hegunhee.ui_component.R.drawable.ic_star_24, SEARCH_KAKAO_ROUTE)
     object Streamer : BottomNavItem(com.hegunhee.ui_component.R.string.streamer,com.hegunhee.ui_component.R.drawable.ic_streamer_24,StreamerNavGraph.streamerRoute)
     object Search : BottomNavItem(com.hegunhee.ui_component.R.string.search,com.hegunhee.resource_common.R.drawable.ic_search_24,SearchNavGraph.searchRoute)
 
