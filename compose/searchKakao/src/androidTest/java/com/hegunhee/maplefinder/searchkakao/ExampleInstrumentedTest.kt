6c49819fd7e25b8b3b3d047cb5378e402fe8ba9f
이건희 <57277631+hegunhee@users.noreply.github.com>
2024-12-08 13:27:24 +0900

    카카오 검색 화면 리팩토링 (#87)
    * #84 - style : SearchKakaoViewModel 코드 자동정렬
    
    ViewModel에는 따로 리팩토링할 코드가 없어보임
    
    * #84 -feat : 카카오 검색 화면에 로딩중, 에러 화면이 나올 수 있도록 적용
    
    * #84 - style : SearchKakaoScreen에 코드 정렬
    
    * #84 - feat : 검색 타입에 디폴트타입 추가
    
    디폴트는 연관순
    
    * #84 - fix : 최신순 검색 버튼이 적용 안되던것 수정
    
    * #84 - UiState Init 추가
    
    해당 화면에 진입할 때 초기 상태는 로딩중이 아닌 초기화이므로
    초기에는 초기화 화면을 보여주기로 결정
    
    * #84 - refactor : 검색타입 텍스트를 상수로 추출
    
    이로인해 다른곳에서도 재사용이 가능해짐
    
    * #84 - feat : 검색 쿼리가 저장되지 않는것을 화면이 이동해도 저장되도록 설정
    
    * #84 - refactor : 공유하기 텍스트 상수로 추출
    
    * #84 - refactor : 딥링크 리스너 위치 이동
    
    * #84 - refactor : 바텀시트 하단의 텍스트들 상수로 추출
    
    * #84 - test : 데이터가 성공적으로 들어왔을 때 화면 테스트

--- compose/searchKakao/src/androidTest/java/com/hegunhee/maplefinder/searchkakao/ExampleInstrumentedTest.kt
+++ compose/searchKakao/src/androidTest/java/com/hegunhee/maplefinder/searchkakao/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hegunhee.maplefinder.searchkakao.test", appContext.packageName)
+        assertEquals("com.hegunhee.compose.searchkakao.test", appContext.packageName)
     }
 }
