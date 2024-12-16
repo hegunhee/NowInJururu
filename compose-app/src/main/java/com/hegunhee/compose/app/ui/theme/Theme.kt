b963ab771a6a604db29e0107540b616b484ffb7d
이건희 <leech9876@naver.com>
2024-12-16 22:34:39 +0900

    #107 - feat : compose-app 다크모드 비활성화

--- compose-app/src/main/java/com/hegunhee/compose/app/ui/theme/Theme.kt
+++ compose-app/src/main/java/com/hegunhee/compose/app/ui/theme/Theme.kt
@@ -39,9 +39,9 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun NowInJururuTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
