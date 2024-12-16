5af653556b05e5f196e426a31892c5beb8529d7d
이건희 <leech9876@naver.com>
2024-12-16 23:06:27 +0900

    #109 - style : ScreenHeaderText의 매개변수에 modifier 추가, 줄 정렬

--- compose/ui-component/src/main/java/com/hegunhee/ui_component/text/ScreenHeaderText.kt
+++ compose/ui-component/src/main/java/com/hegunhee/ui_component/text/ScreenHeaderText.kt
@@ -6,19 +6,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.isSpecified
-import androidx.compose.ui.unit.sp
 import com.hegunhee.resource_common.R
 import com.hegunhee.ui_component.style.headerTextFontSize
 
 @Composable
-fun ScreenHeaderText(text : String) {
-    Text(text = text, fontSize = headerTextFontSize, maxLines = 1,modifier = Modifier.padding(
-        start = dimensionResource(id = R.dimen.header_start_padding),
-        top = dimensionResource(id = R.dimen.header_top_padding),
-        bottom = dimensionResource(id = R.dimen.header_bottom_padding),
-        end = dimensionResource(id = R.dimen.header_end_padding)
-    ))
+fun ScreenHeaderText(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        text = text,
+        fontSize = headerTextFontSize,
+        maxLines = 1,
+        modifier = modifier.padding(
+            start = dimensionResource(id = R.dimen.header_start_padding),
+            top = dimensionResource(id = R.dimen.header_top_padding),
+            bottom = dimensionResource(id = R.dimen.header_bottom_padding),
+            end = dimensionResource(id = R.dimen.header_end_padding)
+        )
+    )
 }
 
 @Preview
